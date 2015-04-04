35386fb1510d0891d1801f072035a0dc448eb2e1
thowes <thw@live.fi>
2015-04-05 00:06:34 +0300

    projekti_go replacing projekti

--- lib/proj.sh
+++ lib/proj.sh
@@ -8,39 +8,6 @@ if [ -f $KR_DIR_LIB/ror.sh ]; then . $KR_DIR_LIB/ror.sh; fi
 if [ -f $KR_DIR_LIB/vs.sh ]; then . $KR_DIR_LIB/vs.sh; fi
 #if [ -f ~/$KR_DIR_LIB/vir.sh ]; then . ~/$KR_DIR_LIB/vir.sh; fi
 if [ $VERBOSITY -ge $LEV_V ]; then tynnyri kick; fi
-PROJECT="def"
-
-projekti() {
-	if [ "$1" == "" ]; then
-		cd ~/$KR_DIR_PROJ; ls
-	else
-		if [ "$1" != "" ] && [ -d ~/$KR_DIR_PROJ/$1 ]; then
-			cd ~/$KR_DIR_PROJ/$1
-			case $2 in
-				info) make info;;
-				mf) nano makefile;;
-				orig) make orig;;
-				origin) make orig;;
-				pull) make pull;;
-				push) make push;;
-				rev) make rev;;
-				st) make st;;
-				status) make st;;
-				tila) make st;;
-				tls) make tls;;
-				todo) make tls;;
-				up) make upd;;
-				upd) make upd;;
-				update) make upd;;
-				*) kaiku PROJ $1; if [ -f makefile ]; then compu_proj; fi;;
-			esac
-		else 
-			if [ "$1" != "" ] && [ "$2" != "" ]; then
-				projekti $2 $1 $3
-			fi
-		fi
-	fi
-}
 
 # Eventually this is going to replace projekti(), but it has some quirks still.
 projekti_go() {
@@ -53,11 +20,11 @@ projekti_go() {
 			jaax st win7
 			jaax ql $VERKKO $1
 			jaax fl $VERKKO $1
+			compu_proj
 		else
 			virhe PROJ $1 $KR_DIRPO "not a directory!"
 		fi
 	else
 		virhe PROJ $1 $KR_DIRPO "just error!"
 	fi
-
 }
