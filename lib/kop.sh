a3fa47701d49a5148d61b94616099bcad1825863
thowes <thw@live.fi>
2015-04-22 10:45:45 +0300

    updating kop.sh

--- lib/kop.sh
+++ lib/kop.sh
@@ -10,11 +10,11 @@ kopioi() {
 		debug KOP $KR_LATEST_TODAY
 		#doesn't do backup runs if already backed up today (checkup happens in bulog)
 		if [ $KR_LATEST_TODAY == "today_false" ]; then
-			mv $KR_DIR_BUT/$2.$USER.$HOSTNAME.gpg $KR_DIR_BUT/$2.$USER.$HOSTNAME.g0
+			rm $KR_DIR_BUT/$2.$USER.$HOSTNAME.z0
 			mv $KR_DIR_BUT/$2.$USER.$HOSTNAME.zip $KR_DIR_BUT/$2.$USER.$HOSTNAME.z0
 			zip -qr $KR_DIR_BUT/$2.$USER.$HOSTNAME.zip $1
 			gpg --encrypt -a -r $RECIPIENT $KR_DIR_BUT/$2.$USER.$HOSTNAME.zip
-			cp $KR_DIR_BUT/$2.$USER.$HOSTNAME.gpg $KR_DIR_BUC/$2.$USER.$HOSTNAME.cbc
+			mv $KR_DIR_BUT/$2.$USER.$HOSTNAME.zip.asc $KR_DIR_BUC/$2.$USER.$HOSTNAME.cbc
 		else
 			debug KOP $2
 		fi
