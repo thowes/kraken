49ddccd4748026cf25f0205738e59718cf4b0dcd
thowes <thw@live.fi>
2015-05-22 23:24:46 +0300

    removing reader.sh, some small changes to proj.sh

--- lib/proj.sh
+++ lib/proj.sh
@@ -2,15 +2,13 @@
 #proj.sh, L 26.6.2013
 if [ $VERBOSITY -ge $LEV_V ]; then tynnyri new "KRAKEN/PROJ /w"; fi
 if [ -f $KR_DIR_LIB/val.sh ]; then . $KR_DIR_LIB/val.sh; fi
-if [ -f $KR_DIR_LIB/svn.sh ]; then . $KR_DIR_LIB/svn.sh; fi
-if [ -f $KR_DIR_LIB/git.sh ]; then . $KR_DIR_LIB/git.sh; fi
 if [ -f $KR_DIR_LIB/ror.sh ]; then . $KR_DIR_LIB/ror.sh; fi
 if [ -f $KR_DIR_LIB/vs.sh ]; then . $KR_DIR_LIB/vs.sh; fi
 #if [ -f ~/$KR_DIR_LIB/vir.sh ]; then . ~/$KR_DIR_LIB/vir.sh; fi
 if [ $VERBOSITY -ge $LEV_V ]; then tynnyri kick; fi
 
-# Eventually this is going to replace projekti(), but it has some quirks still.
-projekti_go() {
+# reads list projects (proj.csv) and jumps to the project folder.
+projekti() {
 	KR_DIRPO=$(cat ~/$KR_DIR_HOST/proj.csv|grep $1|awk -F, '{ print $4 }')
 	if [ $KR_DIRPO != "" ]; then 
 		if [ -d $KR_DIRPO ]; then
