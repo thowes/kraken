7faff52a5ef1759336b4c658e8ac16d58a461f6b
thowes <thw@live.fi>
2015-05-26 17:17:08 +0300

    adding md5 checks to kop.sh, small changes to proj.csv

--- lib/kop.sh
+++ lib/kop.sh
@@ -3,9 +3,17 @@
 if [ $VERBOSITY -ge $LEV_V ]; then tynnyri KOP; fi
 
 kopioi() {
+	varko $1 $2
+}
+
+varko() {
 	if [ -d $1 ] && [ $ != "" ]; then
 		debug KOP $1 $2
 		KR_LATEST_TODAY="today_maybe"
+		# collect md5 checksums of old and new
+		KR_MD5_OLD=$(md5sum $KR_DIR_BUT/$2.$USER.$HOSTNAME.old)
+		KR_MD5_NEW=$(md5sum $KR_DIR_BUT/$2.$USER.$HOSTNAME.zip)
+		# add a comment to backup.log
 		bulog $1 $2.zip original_dir
 		debug KOP $KR_LATEST_TODAY
 		# doesn't do backup runs if already backed up today (checkup happens in bulog)
