7b446a9cdfcfbed5938f37a0bbc2e68b3055798c
thowes <thw@iki.fi>
2016-04-02 00:06:10 +0300

    Change jaax.sh to shorts.sh

--- kraken.sh
+++ kraken.sh
@@ -19,7 +19,7 @@ if [ -f ~/$2 ]; then
 	if [ $VERBOSITY -ge $LEV_V ]; then tynnyri new "KRAKEN/FLM /w"; fi
 	if [ -f $KR_DIR_LIB/vers.sh ]; then . $KR_DIR_LIB/vers.sh; fi
 	if [ -f $KR_DIR_LIB/tccm.sh ]; then . $KR_DIR_LIB/tccm.sh; fi
-	if [ -f $KR_DIR_LIB/jaax.sh ]; then . $KR_DIR_LIB/jaax.sh; fi
+	if [ -f $KR_DIR_LIB/shorts.sh ]; then . $KR_DIR_LIB/shorts.sh; fi
 	if [ -f $KR_DIR_LIB/uus.sh ]; then . $KR_DIR_LIB/uus.sh; fi
 	if [ -f $KR_DIR_LIB/bulog.sh ]; then . $KR_DIR_LIB/bulog.sh; fi
 	if [ -f $KR_DIR_LIB/kop.sh ]; then . $KR_DIR_LIB/kop.sh; fi
