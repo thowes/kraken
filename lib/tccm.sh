4ac452ffa3f47ce25bdd215389b1294d15c7a97f
thowes <thw@iki.fi>
2016-07-23 23:51:14 +0300

    Change tccm.sh to run volume scripts

--- lib/tccm.sh
+++ lib/tccm.sh
@@ -16,16 +16,41 @@ tccm() {
 		dm) tc /q /s /d $2;;
 		dis) tccm d $2;;
 		info) tcc_kr_info;;
-		key) tynnyri $3; tc /l$2 /q /s /m ts /v $3 /p $KR_PASSU /k $4;;
+		key)
+			if [ $VERBOSITY -ge $LEV_V ]; then tynnyri $3; fi
+			tc /l$2 /q /s /m ts /v $3 /p $KR_PASSU /k $4
+			KR_TC_NAME=$(echo $3|awk -F "." '{print $1}')
+			if [ -f /cygdrive/$2/$KR_TC_NAME.sh ]; then echo RUN: /cygdrive/$2/$KR_TC_NAME.sh; else echo NO: /cygdrive/$2/$KR_TC_NAME.sh; fi
+			;;
 		m) tccm mt $2 $3;;
 		mnt) tccm mt $2 $3;;
-		mt) tynnyri $3; tc /l$2 /q /s /m ts /v $3 /p $KR_SALIS /k $4;;
+		mt)
+			if [ $VERBOSITY -ge $LEV_V ]; then tynnyri $3; fi
+			tc /l$2 /q /s /m ts /v $3 /p $KR_SALIS /k $4
+			KR_TC_NAME=$(echo $3|awk -F "." '{print $1}')
+			if [ -f /cygdrive/$2/$KR_TC_NAME.sh ]; then echo RUN: /cygdrive/$2/$KR_TC_NAME.sh; else echo NO: /cygdrive/$2/$KR_TC_NAME.sh; fi
+			;;
 		n) tccm new $2 $3;;
 		new) echo tc /new /v $2 $3;;
 		pwd) tcc_passu;;
-		ro) tynnyri $3; tc /l$2 /q /s /m ro /v $3 /p $KR_PASSU /k $4;;
-		v2) tynnyri $3; tc /l$2 /q /s /v $3 /p $KR_SALIS;;
-		vault) tynnyri $3; tc /l$2 /q /s /v $3 /p $KR_PASSU;;
+		ro)
+			if [ $VERBOSITY -ge $LEV_V ]; then tynnyri $3; fi
+			tc /l$2 /q /s /m ro /v $3 /p $KR_PASSU /k $4
+			KR_TC_NAME=$(echo $3|awk -F "." '{print $1}')
+			if [ -f /cygdrive/$2/$KR_TC_NAME.sh ]; then echo RUN: /cygdrive/$2/$KR_TC_NAME.sh; else echo NO: /cygdrive/$2/$KR_TC_NAME.sh; fi
+			;;
+		v2)
+			if [ $VERBOSITY -ge $LEV_V ]; then tynnyri $3; fi
+			tc /l$2 /q /s /v $3 /p $KR_SALIS
+			KR_TC_NAME=$(echo $3|awk -F "." '{print $1}')
+			if [ -f /cygdrive/$2/$KR_TC_NAME.sh ]; then echo RUN: /cygdrive/$2/$KR_TC_NAME.sh; else echo NO: /cygdrive/$2/$KR_TC_NAME.sh; fi
+			;;
+		vault)
+			if [ $VERBOSITY -ge $LEV_V ]; then tynnyri $3; fi
+			tc /l$2 /q /s /v $3 /p $KR_PASSU
+			KR_TC_NAME=$(echo $3|awk -F "." '{print $1}')
+			if [ -f /cygdrive/$2/$KR_TC_NAME.sh ]; then echo RUN: /cygdrive/$2/$KR_TC_NAME.sh; else echo NO: /cygdrive/$2/$KR_TC_NAME.sh; fi
+			;;
 		wipe) KR_PASSU=passu1; KR_SALIS=passu2;;
 		*) tccm all;;
 	esac
