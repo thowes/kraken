543d2b1c0c649a651095fde7d7fb04579a833ca8
Thomas.Westerberg <thw@iki.fi>
2017-05-03 14:03:06 +0300

    Update project choice function in val.sh to handle empty input

--- lib/val.sh
+++ lib/val.sh
@@ -5,7 +5,16 @@ if [ $VERBOSITY -ge $LEV_V ]; then tynnyri VAL; fi
 valitse() {
 	#prints to screen list projects valid for current location
 	cat ~/$KR_DIR_CFG/proj.csv|grep $1|awk -F, '{ print " "$1"\t" $2 }'
-	KR_PROJ="def"
-	read -p "PROJECT " -r KR_PROJ
-	if [ $KR_PROJ != "" ]; then PROJECT=$KR_PROJ; else virhe PROJ $KR_PROJ; fi
+	KR_PROJ_TEMP="def"
+	KR_PROJ_TEST=" "
+	read -p "PROJECT " -r KR_PROJ_TEMP
+	KR_PROJ_TEST=$(echo $KR_PROJ_TEMP)
+	if [[ -n "${KR_PROJ_TEST/[ ]*\n/}" ]]; then
+		#execute if the the variable is not empty and contains non space characters
+		PROJECT=$KR_PROJ_TEMP
+	else
+    	#execute if the variable is empty or contains only spaces
+		PROJECT="def"
+		virhe PROJ $KR_PROJ_TEST is "empty." 
+	fi
 }
