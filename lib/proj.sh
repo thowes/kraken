d1c0331ca139e5d813330b561412f13ba61abf9e
Thomas.Westerberg <thw@iki.fi>
2017-05-10 13:48:49 +0300

    Update project functions

--- lib/proj.sh
+++ lib/proj.sh
@@ -15,7 +15,7 @@ projekti() {
 	esac
 	if [ $KR_DIRPO != '' ]; then
 		if [ -d $KR_DIRPO ]; then
-			kaiku PROJ $1 $KR_DIRPO
+			debug PROJ $1 $KR_DIRPO
 			cd $KR_DIRPO
 			compu_proj $1
 			#open .
@@ -30,13 +30,14 @@ projekti() {
 # update (pull) project if in master branch, if working dir is clean and if there are no commits to push.
 projekti_update() {
 	if [ $VERKKO != "NADA" ]; then
-		KR_PROJ_BRANCH=$(git status|grep "On branch")
+		KR_PROJ_STATUS=$(git status)
+		KR_PROJ_BRANCH=$(echo $KR_PROJ_STATUS | grep 'On branch')
 		case $KR_PROJ_BRANCH in
 			*master*)
-				KR_PROJ_UPDATE=$(git status|grep origin)
+				KR_PROJ_UPDATE=$(echo $KR_PROJ_STATUS | grep origin)
 				case $KR_PROJ_UPDATE in
 					*up-to-date*)
-						KR_PROJ_COMMITS=$(git status|grep commit|grep to)
+						KR_PROJ_COMMITS=$(echo $KR_PROJ_STATUS | grep commit | grep to)
 						case $KR_PROJ_COMMITS in
 							*nothing*) git pull;;
 							*) virhe You have changes "to commit";;
