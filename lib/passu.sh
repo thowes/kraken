b3f34608c5241fb79080138bf6fdce9372c99868
Thomas.Westerberg <thw@iki.fi>
2017-07-03 12:52:58 +0300

    Update passu.sh

--- lib/passu.sh
+++ lib/passu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #passu.sh, L 7.5.2008/25.3.2013/24.8.2013
-if [ $VERBOSITY -ge $LEV_V ]; then tynnyri PASSU; fi
+if [ $KR_DEBUG == "true" ]; then tynnyri PASSU; fi
 
 passu() {
 	read -p "PAUSE, PRESS ENTER " -r KR_PASSU
