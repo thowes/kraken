f51b4f280b393216ad5e1fc197fcc1fd2bdd8998
Thomas.Westerberg <thw@iki.fi>
2017-07-10 15:21:42 +0300

    Update tccm script

--- lib/tccm_cygwin.sh
+++ lib/tccm_cygwin.sh
@@ -6,6 +6,7 @@ KR_SALIS=passu2
 
 tccm() {
 	debug TTCM $1 $2 $3
+	KR_TC_NAME=$(echo $3|awk -F "." '{print $1}')
 	case $1 in
 		all) tc /q /s /d;;
 		c) tccm mt $2 $3;;
@@ -18,31 +19,26 @@ tccm() {
 		info) tcc_kr_info;;
 		key)
 			tc /l$2 /q /s /m ts /v $3 /p $KR_PASSU /k $4
-			KR_TC_NAME=$(echo $3|awk -F "." '{print $1}')
 			if [ -f /cygdrive/$2/$KR_TC_NAME.sh ]; then . /cygdrive/$2/$KR_TC_NAME.sh; else echo NO: /cygdrive/$2/$KR_TC_NAME.sh; fi
 			;;
 		m) tccm mt $2 $3;;
 		mnt) tccm mt $2 $3;;
 		mt)
 			tc /l$2 /q /s /m ts /v $3 /p $KR_SALIS /k $4
-			KR_TC_NAME=$(echo $3|awk -F "." '{print $1}')
 			if [ -f /cygdrive/$2/$KR_TC_NAME.sh ]; then . /cygdrive/$2/$KR_TC_NAME.sh; else echo NO: /cygdrive/$2/$KR_TC_NAME.sh; fi
 			;;
 		pwd) tcc_passu;;
 		ro)
 			tc /l$2 /q /s /m ro /v $3 /p $KR_PASSU /k $4
-			KR_TC_NAME=$(echo $3|awk -F "." '{print $1}')
 			if [ -f /cygdrive/$2/$KR_TC_NAME.sh ]; then . /cygdrive/$2/$KR_TC_NAME.sh; else echo NO: /cygdrive/$2/$KR_TC_NAME.sh; fi
 			;;
 		rw) tccm key $2 $3 $4;;
 		v2)
 			tc /l$2 /q /s /v $3 /p $KR_SALIS
-			KR_TC_NAME=$(echo $3|awk -F "." '{print $1}')
 			if [ -f /cygdrive/$2/$KR_TC_NAME.sh ]; then . /cygdrive/$2/$KR_TC_NAME.sh; else echo NO: /cygdrive/$2/$KR_TC_NAME.sh; fi
 			;;
 		vault)
 			tc /l$2 /q /s /v $3 /p $KR_PASSU
-			KR_TC_NAME=$(echo $3|awk -F "." '{print $1}')
 			if [ -f /cygdrive/$2/$KR_TC_NAME.sh ]; then . /cygdrive/$2/$KR_TC_NAME.sh; else echo NO: /cygdrive/$2/$KR_TC_NAME.sh; fi
 			;;
 		wipe) KR_PASSU=passu1; KR_SALIS=passu2;;
