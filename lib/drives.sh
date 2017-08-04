ffffe2543d8b3f4cb781a5f772522d5790059b4a
Thomas.Westerberg <thw@iki.fi>
2017-08-04 10:13:50 +0300

    Update drives.sh

--- lib/drives.sh
+++ lib/drives.sh
@@ -3,16 +3,16 @@
 if [ $KR_DEBUG == "true" ]; then tynnyri DRIVES; fi
 
 ker_usb_drives() {
-	if [ $VERBOSITY -ge $LEV_V ]; then tynnyri new DRV; fi
+	if [ $KR_DEBUG == "true" ]; then tynnyri new DRV; fi
 	N=0
 	for f in $(cat $KR_DIR_CFG/usb.lst); do
 		if [ -f $f ]; then
 			N=$(($N+1))
 			. $f
 			if [ $N == 5 ]; then N=0; 
-				if [ $VERBOSITY -ge $LEV_V ]; then tynnyri kick; tynnyri new DRV; fi
+				if [ $KR_DEBUG == "true" ]; then tynnyri kick; tynnyri new DRV; fi
 			fi
 		fi
 	done
-	if [ $VERBOSITY -ge $LEV_V ]; then tynnyri kick; fi
+	if [ $KR_DEBUG == "true" ]; then tynnyri kick; fi
 }
