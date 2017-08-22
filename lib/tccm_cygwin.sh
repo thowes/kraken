2dd3e6355846408407d74d2223f8d75f7df14166
Thomas.Westerberg <thw@iki.fi>
2017-08-22 10:51:55 +0300

    Update tccm.sh

--- lib/tccm_cygwin.sh
+++ lib/tccm_cygwin.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #tccm.sh, L 2.4.2013
 if [ $KR_DEBUG == "true" ]; then tynnyri TCCM; fi
-KR_PASSU=passu1
-KR_SALIS=passu2
+KR_PASSU=passu1; KR_SALIS=passu2
 
 tccm() {
 	debug TTCM $1 $2 $3
@@ -13,32 +12,31 @@ tccm() {
 				darwin) tc -d l$2;;
 			esac;;
 		key) case $KAYTTIS in
-				cygwin) tc /l$2 /q /s /m ts /v $3 /p $KR_PASSU /k $4;;
+				cygwin) tc /l$2 /q /s /m ts /v $3 /p $KR_PASSU /k $4; if [ -f /cygdrive/$2/$KR_TC_NAME.sh ]; then . /cygdrive/$2/$KR_TC_NAME.sh; else echo NO: /cygdrive/$2/$KR_TC_NAME.sh; fi;;
 				darwin) tc -m ts $3 -p $KR_PASSU -k $4 /Volumes/l$2;;
 			esac;;
 		mt) case $KAYTTIS in
-				cygwin) tc /l$2 /q /s /m ts /v $3 /p $KR_SALIS /k $4;;
+				cygwin) tc /l$2 /q /s /m ts /v $3 /p $KR_SALIS /k $4; if [ -f /cygdrive/$2/$KR_TC_NAME.sh ]; then . /cygdrive/$2/$KR_TC_NAME.sh; else echo NO: /cygdrive/$2/$KR_TC_NAME.sh; fi;;
 				darwin) tc -m ts $3 -p $KR_SALIS -k $4 /Volumes/l$2;;
-			esac;;
+			esac;;	
 		nk) case $KAYTTIS in
-				cygwin) tc /l$2 /q /s /v $3 /p $KR_SALIS;;
+				cygwin) tc /l$2 /q /s /v $3 /p $KR_SALIS; if [ -f /cygdrive/$2/$KR_TC_NAME.sh ]; then . /cygdrive/$2/$KR_TC_NAME.sh; else echo NO: /cygdrive/$2/$KR_TC_NAME.sh; fi;;
 				darwin) tc -m ts $3 -p $KR_SALIS /Volumes/l$2;;
 			esac;;
-		pwd) tcc_passu;;
 		ro) case $KAYTTIS in
-				cygwin) tc /l$2 /q /s /m ro /v $3 /p $KR_PASSU /k $4;;
+				cygwin) tc /l$2 /q /s /m ro /v $3 /p $KR_PASSU /k $4; if [ -f /cygdrive/$2/$KR_TC_NAME.sh ]; then . /cygdrive/$2/$KR_TC_NAME.sh; else echo NO: /cygdrive/$2/$KR_TC_NAME.sh; fi;;
 				darwin) tc -m ro $3 -p $KR_PASSU -k $4 /Volumes/l$2;;
 			esac;;
 		rw) case $KAYTTIS in
-				cygwin) tc /l$2 /q /s /m ts /v $3 /p $KR_PASSU /k $4;;
+				cygwin) tc /l$2 /q /s /m ts /v $3 /p $KR_PASSU /k $4; if [ -f /cygdrive/$2/$KR_TC_NAME.sh ]; then . /cygdrive/$2/$KR_TC_NAME.sh; else echo NO: /cygdrive/$2/$KR_TC_NAME.sh; fi;;
 				darwin) tc -m ts $3 -p $KR_PASSU -k $4 /Volumes/l$2;;
 			esac;;
 		v1) case $KAYTTIS in
-				cygwin) tc /l$2 /q /s /v $3 /p $KR_PASSU;;
+				cygwin) tc /l$2 /q /s /v $3 /p $KR_PASSU; if [ -f /cygdrive/$2/$KR_TC_NAME.sh ]; then . /cygdrive/$2/$KR_TC_NAME.sh; else echo NO: /cygdrive/$2/$KR_TC_NAME.sh; fi;;
 				darwin) tc -m ts $3 -p $KR_PASSU /Volumes/l$2;;
 			esac;;
 		v2) case $KAYTTIS in
-				cygwin) tc /l$2 /q /s /v $3 /p $KR_SALIS;;
+				cygwin) tc /l$2 /q /s /v $3 /p $KR_SALIS; if [ -f /cygdrive/$2/$KR_TC_NAME.sh ]; then . /cygdrive/$2/$KR_TC_NAME.sh; else echo NO: /cygdrive/$2/$KR_TC_NAME.sh; fi;;
 				darwin) tc -m ts $3 -p $KR_SALIS /Volumes/l$2;;
 			esac;;
 		wipe) KR_PASSU=passu1; KR_SALIS=passu2;;
@@ -47,5 +45,4 @@ tccm() {
 				darwin) tc -d;;
 			esac;;
 	esac
-	if [ -f /cygdrive/$2/$KR_TC_NAME.sh ]; then . /cygdrive/$2/$KR_TC_NAME.sh; else echo NO: /cygdrive/$2/$KR_TC_NAME.sh; fi
 }
