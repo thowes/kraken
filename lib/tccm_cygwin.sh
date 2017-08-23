4a246b7d41f233323515c23e5fa5c463aa2f1c7c
Thomas.Westerberg <thw@iki.fi>
2017-08-23 10:40:29 +0300

    Update tccm

--- lib/tccm_cygwin.sh
+++ lib/tccm_cygwin.sh
@@ -13,31 +13,31 @@ tccm() {
 			esac;;
 		key) case $KAYTTIS in
 				cygwin) tc /l$2 /q /s /m ts /v $3 /p $KR_PASSU /k $4; if [ -f /cygdrive/$2/$KR_TC_NAME.sh ]; then . /cygdrive/$2/$KR_TC_NAME.sh; else echo NO: /cygdrive/$2/$KR_TC_NAME.sh; fi;;
-				darwin) tc -m ts $3 -p $KR_PASSU -k $4 /Volumes/l$2;;
+				darwin) tc -m ts $3 -p $KR_PASSU -k $4 /Volumes/l$2; if [ -f /Volumes/l$2/$KR_TC_NAME.sh ]; then . /Volumes/l$2/$KR_TC_NAME.sh; else echo NO: /Volumes/l$2/$KR_TC_NAME.sh; fi;;
 			esac;;
 		mt) case $KAYTTIS in
 				cygwin) tc /l$2 /q /s /m ts /v $3 /p $KR_SALIS /k $4; if [ -f /cygdrive/$2/$KR_TC_NAME.sh ]; then . /cygdrive/$2/$KR_TC_NAME.sh; else echo NO: /cygdrive/$2/$KR_TC_NAME.sh; fi;;
-				darwin) tc -m ts $3 -p $KR_SALIS -k $4 /Volumes/l$2;;
+				darwin) tc -m ts $3 -p $KR_SALIS -k $4 /Volumes/l$2; if [ -f /Volumes/l$2/$KR_TC_NAME.sh ]; then . /Volumes/l$2/$KR_TC_NAME.sh; else echo NO: /Volumes/l$2/$KR_TC_NAME.sh; fi;;
 			esac;;	
 		nk) case $KAYTTIS in
 				cygwin) tc /l$2 /q /s /v $3 /p $KR_SALIS; if [ -f /cygdrive/$2/$KR_TC_NAME.sh ]; then . /cygdrive/$2/$KR_TC_NAME.sh; else echo NO: /cygdrive/$2/$KR_TC_NAME.sh; fi;;
-				darwin) tc -m ts $3 -p $KR_SALIS /Volumes/l$2;;
+				darwin) tc -m ts $3 -p $KR_SALIS /Volumes/l$2; if [ -f /Volumes/l$2/$KR_TC_NAME.sh ]; then . /Volumes/l$2/$KR_TC_NAME.sh; else echo NO: /Volumes/l$2/$KR_TC_NAME.sh; fi;;
 			esac;;
 		ro) case $KAYTTIS in
 				cygwin) tc /l$2 /q /s /m ro /v $3 /p $KR_PASSU /k $4; if [ -f /cygdrive/$2/$KR_TC_NAME.sh ]; then . /cygdrive/$2/$KR_TC_NAME.sh; else echo NO: /cygdrive/$2/$KR_TC_NAME.sh; fi;;
-				darwin) tc -m ro $3 -p $KR_PASSU -k $4 /Volumes/l$2;;
+				darwin) tc -m ro $3 -p $KR_PASSU -k $4 /Volumes/l$2; if [ -f /Volumes/l$2/$KR_TC_NAME.sh ]; then . /Volumes/l$2/$KR_TC_NAME.sh; else echo NO: /Volumes/l$2/$KR_TC_NAME.sh; fi;;
 			esac;;
 		rw) case $KAYTTIS in
 				cygwin) tc /l$2 /q /s /m ts /v $3 /p $KR_PASSU /k $4; if [ -f /cygdrive/$2/$KR_TC_NAME.sh ]; then . /cygdrive/$2/$KR_TC_NAME.sh; else echo NO: /cygdrive/$2/$KR_TC_NAME.sh; fi;;
-				darwin) tc -m ts $3 -p $KR_PASSU -k $4 /Volumes/l$2;;
+				darwin) tc -m ts $3 -p $KR_PASSU -k $4 /Volumes/l$2; if [ -f /Volumes/l$2/$KR_TC_NAME.sh ]; then . /Volumes/l$2/$KR_TC_NAME.sh; else echo NO: /Volumes/l$2/$KR_TC_NAME.sh; fi;;
 			esac;;
 		v1) case $KAYTTIS in
 				cygwin) tc /l$2 /q /s /v $3 /p $KR_PASSU; if [ -f /cygdrive/$2/$KR_TC_NAME.sh ]; then . /cygdrive/$2/$KR_TC_NAME.sh; else echo NO: /cygdrive/$2/$KR_TC_NAME.sh; fi;;
-				darwin) tc -m ts $3 -p $KR_PASSU /Volumes/l$2;;
+				darwin) tc -m ts $3 -p $KR_PASSU /Volumes/l$2; if [ -f /Volumes/l$2/$KR_TC_NAME.sh ]; then . /Volumes/l$2/$KR_TC_NAME.sh; else echo NO: /Volumes/l$2/$KR_TC_NAME.sh; fi;;
 			esac;;
 		v2) case $KAYTTIS in
 				cygwin) tc /l$2 /q /s /v $3 /p $KR_SALIS; if [ -f /cygdrive/$2/$KR_TC_NAME.sh ]; then . /cygdrive/$2/$KR_TC_NAME.sh; else echo NO: /cygdrive/$2/$KR_TC_NAME.sh; fi;;
-				darwin) tc -m ts $3 -p $KR_SALIS /Volumes/l$2;;
+				darwin) tc -m ts $3 -p $KR_SALIS /Volumes/l$2; if [ -f /Volumes/l$2/$KR_TC_NAME.sh ]; then . /Volumes/l$2/$KR_TC_NAME.sh; else echo NO: /Volumes/l$2/$KR_TC_NAME.sh; fi;;
 			esac;;
 		wipe) KR_PASSU=passu1; KR_SALIS=passu2;;
 		*)  case $KAYTTIS in
