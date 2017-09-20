15b6655e436680a57a0a3069da6af724f38fcd87
Thomas.Westerberg <thw@iki.fi>
2017-09-20 08:24:56 +0300

    Update variable names in default nets.sh

--- def/nets.sh
+++ def/nets.sh
@@ -4,16 +4,16 @@ if [ $KR_DEBUG == "true" ]; then tynnyri NETS; fi
 
 # When you connect to networks via ethernet cable
 case "$VERKKO_IP" in
-	*0.0.0.0*) VERKKO="NADA";;
-	*192.168.109.*) VERKKO="HOME";;
-	*192.168.1.*) VERKKO="OFFICE";;
+	*0.0.0.0*) KR_NETWORK="NADA"; VERKKO="NADA";;
+	*192.168.109.*) KR_NETWORK="HOME"; VERKKO="HOME";;
+	*192.168.1.*) KR_NETWORK="OFFICE"; VERKKO="OFFICE";;
 esac
 
 # When you connect to networks via WLAN
 case "$VERKKO_SSID" in
-	*cafe_ssid*) VERKKO="CAFE";;
-	*mobile_ssid*) VERKKO="MOB";;
-	*nada*) VERKKO="NADA";;
-	*home_ssid*) VERKKO="HOME";;
-	*office_ssid*) VERKKO="WORK";;
+	*cafe_ssid*) KR_NETWORK="CAFE"; VERKKO="CAFE";;
+	*mobile_ssid*) KR_NETWORK="MOBILE"; VERKKO="MOBILE";;
+	*nada*) KR_NETWORK="NADA"; VERKKO="NADA";;
+	*home_ssid*) KR_NETWORK="HOME"; VERKKO="HOME";;
+	*office_ssid*) KR_NETWORK="WORK"; VERKKO="WORK";;
 esac
