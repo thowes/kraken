13ede851ba3865920ed2f19174c93fbfda0378f0
Thomas.Westerberg <thw@iki.fi>
2017-09-28 09:40:59 +0300

    Update CFG variable in project function

--- lib/proj.sh
+++ lib/proj.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-#proj.sh, L 26.6.2013
 if [ $KR_DEBUG == "true" ]; then tynnyri new "KRAKEN/PROJ /w"; fi
 if [ -f $KR_DIR_LIB/val.sh ]; then . $KR_DIR_LIB/val.sh; fi
-#if [ -f $KR_DIR_LIB/ror.sh ]; then . $KR_DIR_LIB/ror.sh; fi
 if [ $KR_DEBUG == "true" ]; then tynnyri kick; fi
 
 # reads list projects (proj.csv) and jumps to the project folder.
 projekti_go() {
-	KR_DIRPO_TEMP=$(cat ~/$KR_DIR_CFG/proj.csv|grep $1|awk -F, '{ print $4 }')
+	cd
+	KR_DIRPO_TEMP=$(cat $KR_DIR_CFG/proj.csv|grep $1|awk -F, '{ print $4 }')
 	KR_DIRPO=$KR_DIRPO_TEMP
 	KR_DIRPO_HOME=~
 	case $KR_DIRPO_TEMP in
