dc7579f47d6e79d317b7afaeb225d4bdd9477302
Thomas.Westerberg <thw@iki.fi>
2017-10-12 13:35:28 +0300

    Update uus.sh

--- lib/uus.sh
+++ lib/uus.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#uus.sh, L 11.6.2013/11.11.2013
 if [ $KR_DEBUG == "true" ]; then tynnyri "UUS"; fi
 
 uus() {
@@ -10,6 +9,6 @@ uus() {
 		subl) cp $KR_DIR_LIB/../tmpl/subl.sublime-project subl.sublime-project;;
 		svn) cp $KR_DIR_LIB/../tmpl/svn.makefile makefile;;
 		sh) cp $KR_DIR_LIB/../tmpl/template.sh template.sh;;
-		*) cp ~/$KR_DIR_TMPL/$1 $2;;
+		*) cp $KR_DIR_LIB/../tmpl/$1 $2;;
 	esac
 }
