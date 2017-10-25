a726ed4c4f4fe1f0e8f7899f560f863f5e18091f
thowes <thw@iki.fi>
2017-10-26 01:06:47 +0300

    Update synk.sh

--- lib/synk.sh
+++ lib/synk.sh
@@ -3,13 +3,13 @@ if [ $KR_DEBUG == "true" ]; then tynnyri SYNK; fi
 
 synk_real() {
 	debug SYNK real $1 $2
-	if [ -f $KR_DIR_CFG/../loc/sites/$2 ]; then . $KR_DIR_CFG/../loc/sites/$2
+	if [ -f $KR_DIR_SITES/$2 ]; then . $KR_DIR_SITES/$2
 		kaiku $1 $2
 		case $1 in
-			local) echo rsync $SYNK_PARAM $SYNK_DDIR $SYNK_LDIR --exclude-from $KR_DIR_EXCL'/'$SYNK_EXCLUDE;;
-			SSH) ssh $SYNK_USERNAME"@"$SYNK_HOSTNAME;;
+			#local) echo rsync $SYNK_PARAM $SYNK_DDIR $SYNK_LDIR --exclude-from $KR_DIR_EXCL'/'$SYNK_EXCLUDE;;
+			ssh) ssh $SYNK_USERNAME"@"$SYNK_HOSTNAME;;
 			SYN)
-				if [ -f ~/$KR_DIR_EXCL/$2 ]; then
+				if [ -f $KR_DIR_EXCL/$2 ]; then
 					#if [ -f $1/backup.log ]; then tail -n 1 $1/backup.log; fi
 					KR_LATEST_TODAY="today_false"
 					#bulog_add $SYNK_LDIR $HOSTNAME $SYNK_HOSTNAME
@@ -21,12 +21,12 @@ synk_real() {
 					fi
 				else virhe Exclude file missing
 				fi;;
-			upl) rsync $SYNK_PARAM $SYNK_LDIR $SYNK_USERNAME'@'$SYNK_HOSTNAME':'$SYNK_DDIR --exclude-from $KR_DIR_EXCL/$SYNK_EXCLUDE;;
+			upl) rsync $SYNK_PARAM $SYNK_L_UP $SYNK_USERNAME'@'$SYNK_HOSTNAME':'$SYNK_D_UP --exclude-from $KR_DIR_EXCL/$2;;
 			*) virhe SYNK real "-" "no protocol!";;
 		esac
 	else
-		if [ -f $KR_DIR_CFG/../loc/sites/$2.sh ]; then
-			. $KR_DIR_CFG/../loc/sites/$2.sh $1
+		if [ -f $KR_DIR_SITES/$2.sh ]; then
+			. $KR_DIR_SITES/$2.sh $1
 		else
 			virhe SYNK $1 $2 "-" "connection not recognized!"
   	fi
@@ -36,19 +36,19 @@ synk_real() {
 synkronoi() {
 	debug SYNK main $1 $2
 	case $1 in
-		ftp) synk_real SYN $2;;
+		#ftp) synk_real SYN $2;;
 		rsync) synk_real SYN $2;;
-		sftp) synk_real SYN $2;;
+		#sftp) synk_real SYN $2;;
 		syn) synk_real SYN $2;;
 		SYN) synk_real SYN $2;;
 		sync) synk_real SYN $2;;
 		SYNC) synk_real SYN $2;;
 		synk) synk_real SYN $2;;
 		SYNK) synk_real SYN $2;;
-		ssh) synk_real SSH $2;;
-		SSH) synk_real SSH $2;;
+		ssh) synk_real ssh $2;;
+		SSH) synk_real ssh $2;;
 		upl) synk_real upl $2;;
-		yh) synk_real SSH $2;;
+		#yh) synk_real SSH $2;;
 		*) synk_real SYN $1;;
 	esac
 }
