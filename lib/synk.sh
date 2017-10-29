3dfeac109838ecd8839ab183d826c836ba9ca549
thowes <thw@iki.fi>
2017-10-29 10:07:22 +0200

    Update sync functions

--- lib/synk.sh
+++ lib/synk.sh
@@ -2,47 +2,43 @@
 if [ $KR_DEBUG == "true" ]; then tynnyri SYNK; fi
 
 synk_real() {
-	debug SYNK real $1 $2
+	kaiku SYNK real $1 $2
 	if [ -f $KR_DIR_SITES/$2 ]; then . $KR_DIR_SITES/$2
-		kaiku $1 $2
 		case $1 in
 			ssh) ssh $SYNK_USERNAME"@"$SYNK_HOSTNAME;;
 			syn) if [ -d $SYNK_LDIR ]; then if [ -f $KR_DIR_EXCL/$2 ]; then
-					#if [ -f $1/backup.log ]; then tail -n 1 $1/backup.log; fi
-					KR_LATEST_TODAY="today_false"
-					#bulog_add $SYNK_LDIR $HOSTNAME $SYNK_HOSTNAME
+					# Getting the last row of backup.log and splitting it to retrieve the part of the string for latest backup date.
+					BU_LATEST_DATE=$(bulog_latest $SYNK_LDIR|awk '{print $1}')
+					BU_TODAY_DATE=$(date +"%F")
+					# Compare the date in latest line of backup.log, if not today, then continue.
 					#doesn't do backup runs if already backed up today (checkup happens in bulog)
-					if [ $KR_LATEST_TODAY == "today_false" ]; then
+					if [ $BU_LATEST_DATE != $BU_TODAY_DATE ]; then
+						bulog_add $SYNK_LDIR $SYNK_HOSTNAME $HOSTNAME
 						rsync $SYNK_PARAM $SYNK_USERNAME@$SYNK_HOSTNAME:$SYNK_DDIR $SYNK_LDIR --exclude-from $KR_DIR_EXCL/$2
 					else
-						debug KOP $2
+						virhe "ALREADY done backup today" $2
 					fi
-				else virhe NOT FOUND exclude file; fi; else virhe NOT FOUND local dir; fi;;
-			upl) rsync $SYNK_PARAM $SYNK_L_UP $SYNK_USERNAME'@'$SYNK_HOSTNAME':'$SYNK_D_UP --exclude-from $KR_DIR_EXCL/$2;;
+				else virhe "NOT FOUND" exclude file; fi; else virhe "NOT FOUND" local dir $SYNK_LDIR; fi;;
+			upl) if [ -d $SYNK_L_UPL_DIR ]; then if [ -f $KR_DIR_EXCL/$2 ]; then
+					rsync $SYNK_PARAM $SYNK_L_UPL_DIR $SYNK_USERNAME'@'$SYNK_HOSTNAME':'$SYNK_R_UPL_DIR --exclude-from $KR_DIR_EXCL/$2
+				else virhe "NOT FOUND" exclude file; fi; else virhe "NOT FOUND" local dir $SYNK_L_UPL_DIR; fi;;
 			*) synk_real syn $2;; #virhe SYNK real "-" "no protocol!";;
 		esac
 	else
 		if [ -f $KR_DIR_SITES/$2.sh ]; then
 			. $KR_DIR_SITES/$2.sh $1
 		else
-			virhe SYNK $1 $2 "-" "connection not recognized!"
-  	fi
-  fi
+			virhe SYNK $1 $2 "connection not recognized!"
+		fi
+	fi
 }
 
 synkronoi() {
-	kaiku SYNK main $1 $2
 	case $1 in
-		#rsync) synk_real SYN $2;;
-		syn) synk_real SYN $2;;
+		syn) synk_real syn $2;;
 		SYN) synk_real SYN $2;;
-		#sync) synk_real SYN $2;;
-		#SYNC) synk_real SYN $2;;
-		#synk) synk_real SYN $2;;
-		#SYNK) synk_real SYN $2;;
 		ssh) synk_real ssh $2;;
-		#SSH) synk_real ssh $2;;
 		upl) synk_real upl $2;;
-		*) synk_real SYN $1;;
+		*) synk_real syn $1;;
 	esac
 }
