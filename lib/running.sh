8e6981dd149f69520167b57889b3e635c9576a0c
thowes <thw@iki.fi>
2017-11-15 15:51:41 +0200

    Add running function to test if program is being run in the system

---
+++ lib/running.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+running() {
+	if [ -f $KR_DIR_TEMP/tl.lst ]; then
+		KR_APPCHECKER_RUNNING=$(cat -v $KR_DIR_TEMP/tl.lst | grep $1.exe)
+		KR_OUTPUT="false"
+		case $KR_APPCHECKER_RUNNING in
+			*Console*) KR_OUTPUT="true";;
+			*Session*) KR_OUTPUT="true";;
+			*Services*) KR_OUTPUT="true";;
+		esac
+		echo $KR_OUTPUT
+	else
+		case $KAYTTIS in
+			cygwin) tasklist | tr -s " " | awk '{print $1 " " $3}' > $KR_DIR_TEMP/tl.lst;;
+			*) ps aux | tr -s " " | awk '{print $1 " " $8}' > $KR_DIR_TEMP/tl.lst;;
+		esac
+		running $1
+		if [ -f $KR_DIR_TEMP/tl.lst ]; then rm $KR_DIR_TEMP/tl.lst; fi
+	fi
+}
