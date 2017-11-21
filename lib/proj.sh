376162d0b496d567920039044d83fbd5f5257c09
thowes <thw@iki.fi>
2017-11-22 00:24:46 +0200

    Fix project update function in mac

--- lib/proj.sh
+++ lib/proj.sh
@@ -35,15 +35,15 @@ projekti_update() {
 		*master*)
 			KR_PROJ_UPDATE=$(echo $KR_PROJ_STATUS | \grep origin)
 			case $KR_PROJ_UPDATE in
-				*'up to date'*)
+				*date*)
 					KR_PROJ_COMMITS=$(echo $KR_PROJ_STATUS | \grep commit | \grep to)
 					case $KR_PROJ_COMMITS in
-						*nothing*) if [ $KR_NETWORK != "NADA" ]; then git pull; else virhe "Can't pull," "Internet not available"; fi;;
-						*) virhe "Can't" "pull," "You have changes" "to commit";;
+						*nothing*) if [ $KR_NETWORK != "NADA" ]; then git pull; else virhe "Can't update," "Internet not available"; fi;;
+						*) virhe "Can't" "update," "you have changes" "to commit";;
 					esac;;
-				*ahead*) virhe "Can't pull," "You are" "ahead of master" "in commits";;
+				*ahead*) virhe "Can't update," "you are" "ahead of master" "in commits";;
 				*) virhe NOT "up to date" with master;;
 			esac;;
-		*) virhe "Can't pull," "you are" NOT "in master branch";;
+		*) virhe "Can't update," "you are" NOT "in master branch";;
 	esac
 }
