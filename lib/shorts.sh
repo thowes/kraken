ed0c264a449b3a61f6a28e6f63db724cc8632912
thowes <thw@iki.fi>
2018-01-01 19:21:36 +1100

    Update shorts.sh

--- lib/shorts.sh
+++ lib/shorts.sh
@@ -6,19 +6,19 @@ shorts_dt() {
 	case $1 in
 		clr)
 			if [ -d $KR_DIR_BUA/BU.$HOSTNAME/DESKTOP/ ]; then
-				if [ -f $KR_DIR_DT/$KR_NAME_LNK ]; then cp $KR_DIR_DT/*.lnk $KR_DIR_BUA/BU.$HOSTNAME/DESKTOP/; rm $KR_DIR_DT/*.lnk; fi
-				if [ -f $KR_DIR_DT/$KR_NAME_URL ]; then cp $KR_DIR_DT/*.url $KR_DIR_BUA/BU.$HOSTNAME/DESKTOP/; rm $KR_DIR_DT/*.url; fi
+				if [ -f $KR_DIR_DT/$KR_NAME_LNK ]; then \cp $KR_DIR_DT/*.lnk $KR_DIR_BUA/BU.$HOSTNAME/DESKTOP/; \rm $KR_DIR_DT/*.lnk; fi
+				if [ -f $KR_DIR_DT/$KR_NAME_URL ]; then \cp $KR_DIR_DT/*.url $KR_DIR_BUA/BU.$HOSTNAME/DESKTOP/; \rm $KR_DIR_DT/*.url; fi
 			fi;;
-		c) if [ -d $KR_DIR_LNK/COMP/$2 ]; then cp $KR_DIR_LNK/COMP/$2/*.* $KR_DIR_DT/; else virhe SHORTS dt $1 $2; fi;;
-		l) if [ -d $KR_DIR_LNK/KTXT/$2 ]; then cp $KR_DIR_LNK/KTXT/$2/*.* $KR_DIR_DT/; else virhe SHORTS dt $1 $2; fi;;
+		c) if [ -d $KR_DIR_LNK/COMP/$2 ]; then \cp $KR_DIR_LNK/COMP/$2/*.* $KR_DIR_DT/; else virhe SHORTS dt $1 $2; fi;;
+		l) if [ -d $KR_DIR_LNK/KTXT/$2 ]; then \cp $KR_DIR_LNK/KTXT/$2/*.* $KR_DIR_DT/; else virhe SHORTS dt $1 $2; fi;;
 		p)
-			if [ -d $KR_DIR_LNK/PROG/$2 ]; then cp $KR_DIR_LNK/PROG/$2/*.* $KR_DIR_DT/; else virhe SHORTS dt-prog $1 $2; fi
-			if [ -d $KR_DIR_LNK/PROJ/$2 ]; then cp $KR_DIR_LNK/PROJ/$2/*.* $KR_DIR_DT/; else virhe SHORTS dt-proj $1 $2; fi
+			if [ -d $KR_DIR_LNK/PROG/$2 ]; then \cp $KR_DIR_LNK/PROG/$2/*.* $KR_DIR_DT/; else virhe SHORTS dt-prog $1 $2; fi
+			if [ -d $KR_DIR_LNK/PROJ/$2 ]; then \cp $KR_DIR_LNK/PROJ/$2/*.* $KR_DIR_DT/; else virhe SHORTS dt-proj $1 $2; fi
 			;;
-		u) if [ -d $KR_DIR_LNK/USER/$2 ]; then cp $KR_DIR_LNK/USER/$2/*.* $KR_DIR_DT/; else virhe SHORTS dt $1 $2; fi;;
+		u) if [ -d $KR_DIR_LNK/USER/$2 ]; then \cp $KR_DIR_LNK/USER/$2/*.* $KR_DIR_DT/; else virhe SHORTS dt $1 $2; fi;;
 		*)
-			if [ -d $KR_DIR_LNK/PROG/$1 ]; then cp $KR_DIR_LNK/PROG/$1/*.* $KR_DIR_DT/; else virhe SHORTS dt-prog $1 $2; fi
-			if [ -d $KR_DIR_LNK/PROJ/$1 ]; then cp $KR_DIR_LNK/PROJ/$1/*.* $KR_DIR_DT/; else virhe SHORTS dt-proj $1 $2; fi
+			if [ -d $KR_DIR_LNK/PROG/$1 ]; then \cp $KR_DIR_LNK/PROG/$1/*.* $KR_DIR_DT/; else virhe SHORTS dt-prog $1 $2; fi
+			if [ -d $KR_DIR_LNK/PROJ/$1 ]; then \cp $KR_DIR_LNK/PROJ/$1/*.* $KR_DIR_DT/; else virhe SHORTS dt-proj $1 $2; fi
 			;;
 	esac
 }
@@ -28,10 +28,10 @@ shorts_menu() {
 	case $1 in
 		clr)
 			if [ -d $KR_DIR_BUA/BU.$HOSTNAME/MENU/ ]; then
-				if [ -d $KR_DIR_AMENU ]; then cp $KR_DIR_AMENU/*.lnk $KR_DIR_BUA/BU.$HOSTNAME/MENU/; else virhe shorts.sh:28 dir $KR_DIR_AMENU missing; fi
-				if [ -d $KR_DIR_UMENU ]; then cp $KR_DIR_UMENU/*.lnk $KR_DIR_BUA/BU.$HOSTNAME/MENU/; else virhe shorts.sh:29 dir $KR_DIR_UMENU missing; fi
-				if [ -d $KR_DIR_CMENU ]; then cp $KR_DIR_CMENU/*.lnk $KR_DIR_BUA/BU.$HOSTNAME/CMENU/; else virhe shorts.sh:29 dir $KR_DIR_CMENU missing; fi
-				if [ -d $KR_DIR_PMENU ]; then cp $KR_DIR_PMENU/*.lnk $KR_DIR_BUA/BU.$HOSTNAME/PMENU/; else virhe shorts.sh:29 dir $KR_DIR_PMENU missing; fi
+				if [ -d $KR_DIR_AMENU ]; then \cp $KR_DIR_AMENU/*.lnk $KR_DIR_BUA/BU.$HOSTNAME/MENU/; else virhe shorts.sh:28 dir $KR_DIR_AMENU missing; fi
+				if [ -d $KR_DIR_UMENU ]; then \cp $KR_DIR_UMENU/*.lnk $KR_DIR_BUA/BU.$HOSTNAME/MENU/; else virhe shorts.sh:29 dir $KR_DIR_UMENU missing; fi
+				if [ -d $KR_DIR_CMENU ]; then \cp $KR_DIR_CMENU/*.lnk $KR_DIR_BUA/BU.$HOSTNAME/CMENU/; else virhe shorts.sh:29 dir $KR_DIR_CMENU missing; fi
+				if [ -d $KR_DIR_PMENU ]; then \cp $KR_DIR_PMENU/*.lnk $KR_DIR_BUA/BU.$HOSTNAME/PMENU/; else virhe shorts.sh:29 dir $KR_DIR_PMENU missing; fi
 			else virhe shorts.sh:30 dir $KR_DIR_BUA/BU.$HOSTNAME/MENU/ missing
 			fi;;
 	esac
@@ -41,9 +41,9 @@ shorts_sendto() {
 	debug "SHORTS/SENDTO" "$1 $2"
 	if [ -d $KR_DIR_SENDTO ]; then
 		case $1 in
-			clr) rm $KR_DIR_SENDTO/*.lnk;;
-			def) cp $KR_DIR_LNK/SENDTO/default/*.lnk $KR_DIR_SENDTO/;;
-			*) if [ -d $KR_DIR_BUA/BU.$HOSTNAME/SENDTO/$1 ]; then cp $KR_DIR_BUA/SENDTO/$1/*.lnk $KR_DIR_SENDTO/; else virhe SHORTS/SENDTO $1 $2; fi;;
+			clr) \rm $KR_DIR_SENDTO/*.lnk;;
+			def) \cp $KR_DIR_LNK/SENDTO/default/*.lnk $KR_DIR_SENDTO/;;
+			*) if [ -d $KR_DIR_BUA/BU.$HOSTNAME/SENDTO/$1 ]; then \cp $KR_DIR_BUA/SENDTO/$1/*.lnk $KR_DIR_SENDTO/; else virhe SHORTS/SENDTO $1 $2; fi;;
 		esac
 	else
 		virhe SHORTS no SENDTO directory 
@@ -55,10 +55,10 @@ shorts_startup() {
 	case $1 in
 		clr)
 			if [ -f $KR_DIR_STARTUP/$KR_NAME_STARTUP ]; then 
-				cp $KR_DIR_STARTUP/*.lnk $KR_DIR_BUA/BU.$HOSTNAME/STARTUP/; rm $KR_DIR_STARTUP/*.lnk
+				\cp $KR_DIR_STARTUP/*.lnk $KR_DIR_BUA/BU.$HOSTNAME/STARTUP/; \rm $KR_DIR_STARTUP/*.lnk
 			fi;;
-		def) cp $KR_DIR_LNK/STARTUP/default/*.lnk $KR_DIR_STARTUP/;;
-		*) if [ -d $KR_DIR_LNK/STARTUP/$1 ]; then cp $KR_DIR_LNK/STARTUP/$1/*.lnk $KR_DIR_STARTUP/; fi;;
+		def) \cp $KR_DIR_LNK/STARTUP/default/*.lnk $KR_DIR_STARTUP/;;
+		*) if [ -d $KR_DIR_LNK/STARTUP/$1 ]; then \cp $KR_DIR_LNK/STARTUP/$1/*.lnk $KR_DIR_STARTUP/; fi;;
 	esac
 }
 
