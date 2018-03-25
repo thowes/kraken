045c25c7bc380408e3afe5eac0670e361ab523b3
thowes <thw@iki.fi>
2018-03-25 22:22:44 +0300

    Update backup log function

--- lib/bulog.sh
+++ lib/bulog.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+# bulog.sh: functions to manage backup log files.
+
+# returns the last row of the backup log file.
 bulog_latest() {
-	#debug BULOG latest $1
 	if [ -f $1/backup.log ]; then
 		KR_LATEST_BACKUP_LINE=$(tail -n 1 $1/backup.log)
 		echo $KR_LATEST_BACKUP_LINE
 	else
 		touch $1/backup.log
-		KR_MD5_ZERO=
 		if [ -f $1/backup.log ]; then
 			KR_NEW_BACKUP=$(date +"%F %R")
 			echo $KR_NEW_BACKUP $2 ver $3 > $1/backup.log
@@ -16,6 +17,17 @@ bulog_latest() {
 	fi
 }
 
+# returns the 2nd last row of the backup log file.
+bulog_2nd_latest() {
+	if [ -f $1/backup.log ]; then
+		KRN_2ND_LATEST_LINE=$(tail -n 2 $1/backup.log)
+		echo $KRN_2ND_LATEST_LINE
+	else
+		virhe $1/backup.log "doesn't exist."
+	fi
+}
+
+# adds a line to the backup log file.
 bulog_add() {
 	#debug BULOG add $1 $2 $3
 	if [ -f $1/backup.log ]; then
@@ -26,7 +38,7 @@ bulog_add() {
 		touch $1/backup.log
 		if [ -f $1/backup.log ]; then
 			KR_NEW_BACKUP=$(date +"%F %R")
-			echo $KR_NEW_BACKUP new ver 0 > $1/backup.log
+			echo $KR_NEW_BACKUP $2 new $3 > $1/backup.log
 		else
 			virhe $1/backup.log is not a valid file.
 		fi
