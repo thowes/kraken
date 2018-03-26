043078d90b4cd0635bc6e9ded358bf14337fd6c9
thowes <thw@iki.fi>
2018-03-26 12:31:18 +0300

    Update running function

--- lib/running.sh
+++ lib/running.sh
@@ -4,14 +4,14 @@ running() {
 		KR_OUTPUT=false
 		case $KAYTTIS in
 			cygwin)
-				KR_APPCHECKER_RUNNING=$(tasklist | tr -s " " | awk '{print $1 " " $3}' | grep $1.exe)
+				KR_APPCHECKER_RUNNING=$(tasklist | tr -s " " | awk '{print $1 " " $3}' | \grep $1.exe)
 				case $KR_APPCHECKER_RUNNING in
 					*Console*) KR_OUTPUT=true;;
 					*Session*) KR_OUTPUT=true;;
 					*Services*) KR_OUTPUT=true;;
 				esac;;
 			*) # not functional yet
-				KR_APPCHECKER_NUMBER=$(ps aux | grep -v grep | grep $1 | wc -l)
+				KR_APPCHECKER_NUMBER=$(ps aux | \grep -v grep | \grep $1 | wc -l)
 				case "$KR_APPCHECKER_NUMBER" in
 					*0) KR_OUTPUT=false;;
 					*) KR_OUTPUT=true;;
@@ -24,6 +24,6 @@ running() {
 			*) ps aux | tr -s " " | awk '{print $11 $12}' > $KR_DIR_TEMP/tl.lst;;
 		esac
 		running $1
-		if [ -f $KR_DIR_TEMP/tl.lst ]; then rm $KR_DIR_TEMP/tl.lst; fi
+		if [ -f $KR_DIR_TEMP/tl.lst ]; then \rm $KR_DIR_TEMP/tl.lst; fi
 	fi
 }
