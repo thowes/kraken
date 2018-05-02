cdf2f5b457108b0a65635a3dbcf1bf76afa8c6de
thowes <thw@iki.fi>
2018-05-02 10:53:21 +0300

    Update short.sh

--- lib/shorts.sh
+++ lib/shorts.sh
@@ -5,9 +5,9 @@ shorts_dt() {
 		c) if [ -d $KR_DIR_LNK/COMP/$2 ]; then \cp $KR_DIR_LNK/COMP/$2/*.* $KR_DIR_DT/; else virhe SHORTS dt $1 $2; fi;;
 		l) if [ -d $KR_DIR_LNK/KTXT/$2 ]; then \cp $KR_DIR_LNK/KTXT/$2/*.* $KR_DIR_DT/; else virhe SHORTS dt $1 $2; fi;;
 		p)
-			if [ -d $KR_DIR_LNK/PROG/$2 ]; then \cp $KR_DIR_LNK/PROG/$2/*.* $KR_DIR_DT/; else virhe SHORTS "dir dt-prog" $1 $2; fi
-			if [ -d $KR_DIR_LNK/PROJ/$2 ]; then \cp $KR_DIR_LNK/PROJ/$2/*.* $KR_DIR_DT/; else virhe SHORTS "dir dt-proj" $1 $2; fi
-			if [ -d $KR_DIRPO/.desktop_links ]; then \cp $KR_DIRPO/.desktop_links/*.* $KR_DIR_DT/; else virhe DIR .desktop_links "not found"; fi
+			if [ -d $KR_DIR_LNK/PROG/$2 ]; then \cp $KR_DIR_LNK/PROG/$2/*.* $KR_DIR_DT/; else virhe "SHORTS dir dt-prog" $1 $2 "not found."; fi
+			if [ -d $KR_DIR_LNK/PROJ/$2 ]; then \cp $KR_DIR_LNK/PROJ/$2/*.* $KR_DIR_DT/; else virhe "SHORTS dir dt-proj" $1 $2 "not found."; fi
+			if [ -d $KR_DIRPO/.desktop_links ]; then \cp $KR_DIRPO/.desktop_links/*.* $KR_DIR_DT/; else virhe DIR .desktop_links "not found."; fi
 			;;
 		u) if [ -d $KR_DIR_LNK/USER/$2 ]; then \cp $KR_DIR_LNK/USER/$2/*.* $KR_DIR_DT/; else virhe SHORTS dt $1 $2; fi;;
 		*) shorts_dt p $1;;
