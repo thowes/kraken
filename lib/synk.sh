1d1dcec0e07a306ef8b53c321f827cef3ad1e114
thowes <thw@iki.fi>
2018-05-31 10:59:16 +0300

    Update sync function

--- lib/synk.sh
+++ lib/synk.sh
@@ -49,22 +49,22 @@ synkronoi() {
 				case $(cat $KR_DIR_CFG/dwl.csv | \grep $2 | wc -l) in
 					0) virhe "information not found.";;
 					1) KR_SYNK_LINE=$(cat $KR_DIR_CFG/dwl.csv | \grep $2)
-						KR_SYNK_EXCL=$(echo $KR_SYNK_LINE|awk -F\; '{print $1}').lst
-						if [ -f $KR_DIR_EXCL/$KR_SYNK_EXCL ]; then debug "file exists."; else KR_SYNK_EXCL=default.lst; fi
+						KR_SYNK_EXCL=$KR_DIR_EXCL/$(echo $KR_SYNK_LINE|awk -F\; '{print $1}').lst
+						if [ -f $KR_SYNK_EXCL ]; then debug "file exists."; else KR_SYNK_EXCL=$KR_DIR_EXCL/default.lst; fi
 						KR_SYNK_SERVER=$(echo $KR_SYNK_LINE|awk -F\; '{print $2}')
 						KR_SYNK_USER=$(echo $KR_SYNK_LINE|awk -F\; '{print $3}')
 						KR_SYNK_PARAM=$(echo $KR_SYNK_LINE|awk -F\; '{print $4}')
 						KR_SYNK_LDIR=$(echo $KR_SYNK_LINE|awk -F\; '{print $5}')
 						KR_SYNK_RDIR=$(echo $KR_SYNK_LINE|awk -F\; '{print $6}')
 						kaiku SY $1 $2 $KR_SYNK_USER@$KR_SYNK_SERVER
-						echo rsync $KR_SYNK_PARAM $KR_SYNK_USER@$KR_SYNK_SERVER:$KR_SYNK_RDIR $KR_SYNK_LDIR --exclude-from $KR_DIR_EXCL/$KR_SYNK_EXCL
+						echo rsync $KR_SYNK_PARAM $KR_SYNK_USER@$KR_SYNK_SERVER:$KR_SYNK_RDIR $KR_SYNK_LDIR --exclude-from $KR_SYNK_EXCL
 						;;
 					*) virhe "found too many sites.";;
 				esac; else virhe "csv file not found."; fi;;
 		ssh) if [ -f $KR_DIR_CFG/dwl.csv ] && [ -f $KR_DIR_CFG/upl.csv ]; then
 				case $(cat $KR_DIR_CFG/*l.csv | \grep $2 | wc -l) in
 					0) virhe "information not found.";;
-					1) KR_SYNK_LINE=$(cat $KR_DIR_CFG/*loads.csv | \grep $2)
+					1) KR_SYNK_LINE=$(cat $KR_DIR_CFG/*l.csv | \grep $2)
 						KR_SYNK_SERVER=$(echo $KR_SYNK_LINE|awk -F\; '{print $2}')
 						KR_SYNK_USER=$(echo $KR_SYNK_LINE|awk -F\; '{print $3}')
 						kaiku SY $1 $2 $KR_SYNK_USER@$KR_SYNK_SERVER
@@ -78,15 +78,15 @@ synkronoi() {
 				case $(cat $KR_DIR_CFG/upl.csv | \grep $2 | wc -l) in
 					0) virhe "information not found.";;
 					1) KR_SYNK_LINE=$(cat $KR_DIR_CFG/upl.csv | \grep $2)
-						KR_SYNK_EXCL=$(echo $KR_SYNK_LINE|awk -F\; '{print $1}').lst
-						if [ -f $KR_DIR_EXCL/$KR_SYNK_EXCL ]; then debug "file exists."; else KR_SYNK_EXCL=default.lst; fi
+						KR_SYNK_EXCL=$KR_DIR_EXCL/$(echo $KR_SYNK_LINE|awk -F\; '{print $1}').lst
+						if [ -f $KR_SYNK_EXCL ]; then debug "file exists."; else KR_SYNK_EXCL=$KR_DIR_EXCL/default.lst; fi
 						KR_SYNK_SERVER=$(echo $KR_SYNK_LINE|awk -F\; '{print $2}')
 						KR_SYNK_USER=$(echo $KR_SYNK_LINE|awk -F\; '{print $3}')
 						KR_SYNK_PARAM=$(echo $KR_SYNK_LINE|awk -F\; '{print $4}')
 						KR_SYNK_LDIR=$(echo $KR_SYNK_LINE|awk -F\; '{print $5}')
 						KR_SYNK_RDIR=$(echo $KR_SYNK_LINE|awk -F\; '{print $6}')
 						kaiku SY $1 $2 $KR_SYNK_USER@$KR_SYNK_SERVER
-						rsync $KR_SYNK_PARAM $KR_SYNK_LDIR $KR_SYNK_USER@$KR_SYNK_SERVER:$KR_SYNK_RDIR --exclude-from $KR_DIR_EXCL/$KR_SYNK_EXCL
+						echo rsync $KR_SYNK_PARAM $KR_SYNK_LDIR $KR_SYNK_USER@$KR_SYNK_SERVER:$KR_SYNK_RDIR --exclude-from $KR_SYNK_EXCL
 						;;
 					*) virhe "found too many sites.";;
 				esac; else virhe "csv file not found."; fi;;
