4b4826fc75eec6b1cd88aacc776df82172b68898
thowes <thw@iki.fi>
2018-06-19 12:55:19 +0300

    Remove old stuff from project update function

--- lib/proj.sh
+++ lib/proj.sh
@@ -59,28 +59,11 @@ projekti_status() {
 }
 
 # this function is a tool for keeping projects automatically updated if they use git.
-# update (pull) project if in master branch, if working dir is clean and if there are no commits to push.
+# update (pull) project if locally up to date.
 projekti_update() {
 	KRN_PROJ_ST_NEW=$(projekti_status)
 	case $KRN_PROJ_ST_NEW in
-		*Up-to-date*)  if [ $KR_NETWORK != "NADA" ]; then kaiku "PROJ_UPD:" $KRN_PROJ_ST_NEW; else virhe "PROJ_UPD:" "Can't update," "Internet not available"; fi;;
+		*Up-to-date*) if [ $KR_NETWORK != "NADA" ]; then kaiku "PROJ_UPD:" $KRN_PROJ_ST_NEW; git pull; else virhe "PROJ_UPD:" "Can't update," "Internet not available"; fi;;
 		*) virhe "PROJ_UPD:" "$KRN_PROJ_ST_NEW";;
 	esac
-	KR_PROJ_STATUS=$(git status)
-	KR_PROJ_BRANCH=$(echo $KR_PROJ_STATUS | \grep 'On branch')
-	#case $KR_PROJ_BRANCH in
-	#	*master*)
-	#		KR_PROJ_UPDATE=$(echo $KR_PROJ_STATUS | \grep origin)
-	#		case $KR_PROJ_UPDATE in
-	#			*date*)
-	#				KR_PROJ_COMMITS=$(echo $KR_PROJ_STATUS | \grep commit | \grep to)
-	#				case $KR_PROJ_COMMITS in
-	#					*nothing*) if [ $KR_NETWORK != "NADA" ]; then git pull; else virhe "Can't update," "Internet not available"; fi;;
-	#					*) virhe "Can't" "update," "you have changes" "to commit";;
-	#				esac;;
-	#			*ahead*) virhe "Can't update," "you are" "ahead of master" "in commits";;
-	#			*) virhe NOT "up to date" with master;;
-	#		esac;;
-	#	*) virhe "Can't update," "you are" NOT "in master branch";;
-	#esac
 }
