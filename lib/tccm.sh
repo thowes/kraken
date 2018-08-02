0b6227577cef18e0e01f860f10de5e75933f61d2
thowes <thw@iki.fi>
2018-08-02 15:27:31 +0300

    Update tccm.sh

--- lib/tccm.sh
+++ lib/tccm.sh
@@ -46,7 +46,7 @@ tccm() {
 				*) tc -m ro $3 -p $KR_PASSU $KRN_TC_DIR/l$2; if [ -f $KRN_TC_DIR/l$2/$KR_TC_NAME.sh ]; then . $KRN_TC_DIR/l$2/$KR_TC_NAME.sh; else echo NO: $KRN_TC_DIR/l$2/$KR_TC_NAME.sh; fi;;
 			esac;;
 		v2) case $KAYTTIS in
-				cygwin) tc /l$2 /q /s /v $3 /p $KR_SALIS; if [ -f /cygdrive/$2/$KR_TC_NAME.sh ]; then . /cygdrive/$2/$KR_TC_NAME.sh; else echo NO: /cygdrive/$2/$KR_TC_NAME.sh; fi;;
+				cygwin) tc /l$2 /q /s /m ts /v $3 /p $KR_SALIS; if [ -f /cygdrive/$2/$KR_TC_NAME.sh ]; then . /cygdrive/$2/$KR_TC_NAME.sh; else echo NO: /cygdrive/$2/$KR_TC_NAME.sh; fi;;
 				*) tc -m ts $3 -p $KR_SALIS $KRN_TC_DIR/l$2; if [ -f $KRN_TC_DIR/l$2/$KR_TC_NAME.sh ]; then . $KRN_TC_DIR/l$2/$KR_TC_NAME.sh; else echo NO: $KRN_TC_DIR/l$2/$KR_TC_NAME.sh; fi;;
 			esac;;
 		wipe) KR_PASSU=passu1; KR_SALIS=passu2;;
