00c1accad7ebd0d9823181a0e5f53be63c257e8e
thowes <thw@iki.fi>
2018-09-19 13:53:20 +0300

    Update tccm function in tccm.sh

--- lib/tccm.sh
+++ lib/tccm.sh
@@ -12,23 +12,23 @@ tccm() {
 	if [ -d $KRN_TC_DIR/$2/ ]; then virhe "Drive directory already in use."; else case $1 in
 		dm) case $KAYTTIS in
 				cygwin) tc /q /s /d $2;;
-				*) tc -d l$2;;
+				*) tc -d $2;;
 			esac;;
 		ro) case $KAYTTIS in
 				cygwin) tc /l$2 /q /s /m ro /v $3 /p $KRN_PASSU /k $4; if [ -f $KRN_TC_DIR/$2/$KR_TC_NAME.sh ]; then . $KRN_TC_DIR/$2/$KR_TC_NAME.sh; else echo NO: $KRN_TC_DIR/$2/$KR_TC_NAME.sh; fi;;
-				*) tc -m ro $3 -p $KRN_PASSU -k $4 $KRN_TC_DIR/l$2; if [ -f $KRN_TC_DIR/l$2/$KR_TC_NAME.sh ]; then . $KRN_TC_DIR/l$2/$KR_TC_NAME.sh; else echo NO: $KRN_TC_DIR/l$2/$KR_TC_NAME.sh; fi;;
+				*) tc -m ro $3 -p $KRN_PASSU -k $4 $KRN_TC_DIR/$2; if [ -f $KRN_TC_DIR/$2/$KR_TC_NAME.sh ]; then . $KRN_TC_DIR/$2/$KR_TC_NAME.sh; else echo NO: $KRN_TC_DIR/$2/$KR_TC_NAME.sh; fi;;
 			esac;;
 		rw) case $KAYTTIS in
 				cygwin) tc /l$2 /q /s /m ts /v $3 /p $KRN_PASSU /k $4; if [ -f $KRN_TC_DIR/$2/$KR_TC_NAME.sh ]; then . $KRN_TC_DIR/$2/$KR_TC_NAME.sh; else echo NO: $KRN_TC_DIR/$2/$KR_TC_NAME.sh; fi;;
-				*) tc -m ts $3 -p $KRN_PASSU -k $4 $KRN_TC_DIR/l$2; if [ -f $KRN_TC_DIR/l$2/$KR_TC_NAME.sh ]; then . $KRN_TC_DIR/l$2/$KR_TC_NAME.sh; else echo NO: $KRN_TC_DIR/l$2/$KR_TC_NAME.sh; fi;;
+				*) tc -m ts $3 -p $KRN_PASSU -k $4 $KRN_TC_DIR/$2; if [ -f $KRN_TC_DIR/$2/$KR_TC_NAME.sh ]; then . $KRN_TC_DIR/$2/$KR_TC_NAME.sh; else echo NO: $KRN_TC_DIR/$2/$KR_TC_NAME.sh; fi;;
 			esac;;
 		vo) case $KAYTTIS in
 				cygwin) tc /l$2 /q /s /m ro /v $3 /p $KRN_PASSU; if [ -f $KRN_TC_DIR/$2/$KR_TC_NAME.sh ]; then . $KRN_TC_DIR/$2/$KR_TC_NAME.sh; else echo NO: $KRN_TC_DIR/$2/$KR_TC_NAME.sh; fi;;
-				*) tc -m ro $3 -p $KRN_PASSU $KRN_TC_DIR/l$2; if [ -f $KRN_TC_DIR/l$2/$KR_TC_NAME.sh ]; then . $KRN_TC_DIR/l$2/$KR_TC_NAME.sh; else echo NO: $KRN_TC_DIR/l$2/$KR_TC_NAME.sh; fi;;
+				*) tc -m ro $3 -p $KRN_PASSU $KRN_TC_DIR/$2; if [ -f $KRN_TC_DIR/$2/$KR_TC_NAME.sh ]; then . $KRN_TC_DIR/$2/$KR_TC_NAME.sh; else echo NO: $KRN_TC_DIR/$2/$KR_TC_NAME.sh; fi;;
 			esac;;
 		vw) case $KAYTTIS in
 				cygwin) tc /l$2 /q /s /m ts /v $3 /p $KRN_PASSU; if [ -f $KRN_TC_DIR/$2/$KR_TC_NAME.sh ]; then . $KRN_TC_DIR/$2/$KR_TC_NAME.sh; else echo NO: $KRN_TC_DIR/$2/$KR_TC_NAME.sh; fi;;
-				*) tc -m ts $3 -p $KRN_PASSU $KRN_TC_DIR/l$2; if [ -f $KRN_TC_DIR/l$2/$KR_TC_NAME.sh ]; then . $KRN_TC_DIR/l$2/$KR_TC_NAME.sh; else echo NO: $KRN_TC_DIR/l$2/$KR_TC_NAME.sh; fi;;
+				*) tc -m ts $3 -p $KRN_PASSU $KRN_TC_DIR/$2; if [ -f $KRN_TC_DIR/$2/$KR_TC_NAME.sh ]; then . $KRN_TC_DIR/$2/$KR_TC_NAME.sh; else echo NO: $KRN_TC_DIR/$2/$KR_TC_NAME.sh; fi;;
 			esac;;
 		wipe) KR_PASSU=passu1; KR_SALIS=passu2;;
 		*)  case $KAYTTIS in
