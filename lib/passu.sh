2e2972616426fc90cb42808a0b355d87dbd64542
thowes <thw@iki.fi>
2018-10-14 18:00:31 +0300

    Remove some old functions

--- lib/passu.sh
+++ lib/passu.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-#passu.sh, L 7.5.2008/25.3.2013/24.8.2013
 if [ $KR_DEBUG == "true" ]; then tynnyri PASSU; fi
-
-passu() {
-	read -p "PAUSE, PRESS ENTER " -r KR_PASSU
-}
-
-salis() {
-	read -p "PRESS ENTER " -r KR_SALIS
-}
