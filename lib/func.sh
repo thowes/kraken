d0e446115ab6413f2703ed49d4356dbceb991922
thowes <thw@iki.fi>
2018-11-04 15:08:53 +0200

    Move pvm function to bin/pvm

--- lib/func.sh
+++ lib/func.sh
@@ -8,25 +8,3 @@ olemassa() {
 	kaiku WHICH $1; which $1
 	kaiku ALIAS $1; alias $1
 }
-
-# pvm is short for päivämäärä ('calendar date' in Finnish).
-# Takes datetime information as input.
-# gives full YYYY-MM-DD HH:MM:SS.xyz as output.
-# the part missing from input is generated based on the current time.
-pvm() {
-	if [ "$1" != "" ]; then
-		if [ "$2" != "" ]; then
-			KR_SEC=$(echo $2 | awk -F\: '{print $3}')
-			if [ "$KR_SEC" != "" ]; then echo $1 $2.$(date +"%N")
-			else echo $1 $2:$(date +"%S.%N"); fi
-		else echo $1 $(date +"%T.%N"); fi
-	else date +"%F %T.%N"; fi
-}
-
-# this function gives md5 and sha256 checksums for individual as output"
-summa() {
-	case $KAYTTIS in
-		darwin) echo $(md5 $1|awk -F= '{print $2}'),$(shasum -a 256 $1|awk '{print $1}');;
-		*) echo $(md5sum $1|awk '{print $1}'),$(sha256sum $1|awk '{print $1}');;
-	esac
-}
