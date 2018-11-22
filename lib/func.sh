b81d5bca1ffe134f6609bb306fbc3ce13c5dd4b7
thowes <thw@iki.fi>
2018-11-22 11:45:34 +0200

    Update lib/func.sh

--- lib/func.sh
+++ lib/func.sh
@@ -3,13 +3,13 @@
 # I use this function to check if a command exists already 
 # as an alias, as an exetable within PATH, or as a man page
 # so that I don't name a new command as something that already exists.
-olemassa() {}
-	if [ "_$1_" != "__" ]; then 
-		kaiku WHICH $1; which $1
-		kaiku MAN $1; man -w $1
-		# this will only work correctly in a function
-		kaiku ALIAS "$1"; \alias "$1"
-	else
-		virhe Missing argument.
-	fi
+olemassa() {
+if [ "_$1_" != "__" ]; then 
+	kaiku WHICH $1; which $1
+	kaiku MAN $1; man -w $1
+	# this will only work correctly in a function
+	kaiku ALIAS "$1"; \alias "$1"
+else
+	virhe Missing argument.
+fi
 }
