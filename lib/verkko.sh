509ea087c5dbb56b113da3dde30ff847fe0be95c
thowes <thw@iki.fi>
2018-12-01 21:38:19 +0200

    Remove extra lines from lib/verkko

--- lib/verkko.sh
+++ lib/verkko.sh
@@ -2,20 +2,16 @@
 if [ -f $KR_DIR_CFG/nets.sh ]; then
 	VERKKO_IP=$(verkko ip)
 	VERKKO_ESSID="NADA"
-	VERKKO_MAC="NADA"
 	# Contents of KAYTTIS variable are defined in ymp.sh, to recognize the OS.
 	# Different network commands are available in different OSes.
 	case $KAYTTIS in
 		cygwin) # Using windows networks commands
-			#VERKKO_IP=$(ipconfig|\grep IPv4)
 			if [ $KR_WIRELESS == "true" ]; then VERKKO_ESSID=$(netsh wlan show interfaces|\grep SSID|\grep -v BSSID); else VERKKO_ESSID=nada; fi
 			;;
 		darwin) # Using macos networks commands.
-			#VERKKO_IP=$(ifconfig|\grep broadcast)
 			if [ $KR_WIRELESS == "true" ]; then VERKKO_ESSID=$(airport -I|\grep SSID|\grep -v BSSID); else VERKKO_ESSID=nada; fi
 			;;
 		ubuntu) # Using unix/linux/ubuntu networks commands.
-			#VERKKO_IP=$(ifconfig eth0|\grep Bcast)
 			if [ $KR_WIRELESS == "true" ]; then VERKKO_ESSID=$(iwlist wlan0 scan|\grep ESSID); else VERKKO_ESSID=nada; fi
 			;;
 	esac
