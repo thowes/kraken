97e5e83b555fc9a04e6242e5bc06a76ae52973d6
thowes <thw@iki.fi>
2018-12-14 23:06:15 +0200

    Edit def/kerain.sh

--- def/kerain.sh
+++ def/kerain.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 if [ $KR_DEBUG == "true" ]; then tynnyri KERAIN; fi
 
-cd
-tynnyri new KERAIN
+if [ -f ~/.bash_logout ]; then cp ~/.bash_logout $KR_DIR_CFG/bash/.bash_logout; fi
+if [ -f ~/.bash_profile ]; then cp ~/.bash_profile $KR_DIR_CFG/bash/.bash_profile; fi
+if [ -f ~/.bashrc ]; then cp ~/.bashrc $KR_DIR_CFG/bash/.bashrc; fi
+if [ -f ~/.profile ]; then cp ~/.profile $KR_DIR_CFG/bash/.profile; fi
+if [ -f ~/.rvmrc ]; then cp ~/.rvmrc $KR_DIR_CFG/bash/.rvmrc; fi
 if [ -f ~/public_html/index.html ]; then cp ~/public_html/index.html $KR_DIR_CFG/server/index.html; fi
-tynnyri kick $1
