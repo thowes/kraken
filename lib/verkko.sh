59f20bd37d73882528e45babe66a7ce56330cff4
thowes <thw@iki.fi>
2018-12-26 17:39:23 +0200

    Edit verkko.sh

--- lib/verkko.sh
+++ lib/verkko.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -f $KR_DIR_CFG/nets.sh ]; then
 	VERKKO_IP=$(verkko ip)
-	VERKKO_ESSID="NADA"
+	if [ $KR_WIRELESS == "true" ]; then VERKKO_ESSID=$(verkko ssid); else VERKKO_ESSID=nada; fi
 	# Contents of KAYTTIS variable are defined in ymp.sh, to recognize the OS.
 	# Different network commands are available in different OSes.
 	case $KAYTTIS in
