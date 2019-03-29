13957db6098d77a6cee715bc6de4f84ecdd7ce3c
thowes <thw@iki.fi>
2019-03-29 12:04:06 +0200

    Remove old tynnyri function from kaiku.sh

--- lib/kaiku.sh
+++ lib/kaiku.sh
@@ -6,21 +6,6 @@ kaiku80() {
 	echo 12345678901234567890123456789012345678901234567890123456789012345678901234567890
 }
 
-#tynnyri() {
-#	case $1 in
-#		a) tynnyri add $2;;
-#		add) TYNNYRI="$TYNNYRI $2,";;
-#		d) tynnyri dump $2;;
-#		del) TYNNYRI="";;
-#		k) tynnyri kick;;
-#		dump) TYNNYRI="$TYNNYRI$2"; tynnyri kick;;
-#		kick) echo "[[ $TYNNYRI ]]"; tynnyri del;;
-#		new) TYNNYRI="$2";;
-#		pr) echo "[[ $TYNNYRI ]]";;
-#		*) tynnyri add $1;;
-#	esac
-#}
-
 verboosi() {
 	if [ $KR_DEBUG == "true" ]; then echo "[[ $1 $2 $3 $4 ]]"; fi
 }
