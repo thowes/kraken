80d95d6ad06ee1885b2beb09c42099f74f31454e
thowes <thw@iki.fi>
2019-04-07 22:37:56 +0300

    Comment out the verboosi function

--- lib/kaiku.sh
+++ lib/kaiku.sh
@@ -6,6 +6,6 @@ kaiku80() {
 	echo 12345678901234567890123456789012345678901234567890123456789012345678901234567890
 }
 
-verboosi() {
-	if [ $KR_DEBUG == "true" ]; then echo "[[ $1 $2 $3 $4 ]]"; fi
-}
+#verboosi() {
+#	if [ $KR_DEBUG == "true" ]; then echo "[[ $1 $2 $3 $4 ]]"; fi
+#}
