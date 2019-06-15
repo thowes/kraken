a36c2a5aa1df8255856f58ed251888c8b4da8ed8
thowes <thw@iki.fi>
2019-06-16 00:37:07 +0300

    Edit kr_network.sh

--- lib/kr_network.sh
+++ lib/kr_network.sh
@@ -6,9 +6,9 @@ if [ $(asetus bool:hosted) != "true" ]; then
 	# Contents of KAYTTIS variable are defined in ymp.sh, to recognize the OS.
 	# Different network commands are available in different OSes.
 	case $KAYTTIS in
-		cygwin) # Using windows networks commands
-			if [ $KR_WIRELESS == "true" ]; then VERKKO_ESSID=$(verkko ssid); else VERKKO_ESSID=nada; fi
-			;;
+		#cygwin) # Using windows networks commands
+			#if [ $KR_WIRELESS == "true" ]; then VERKKO_ESSID=$(verkko ssid); else VERKKO_ESSID=nada; fi
+			#;;
 		darwin) # Using macos networks commands.
 			if [ $KR_WIRELESS == "true" ]; then VERKKO_ESSID=$(verkko ssid); else VERKKO_ESSID=nada; fi
 			;;
@@ -16,7 +16,7 @@ if [ $(asetus bool:hosted) != "true" ]; then
 			if [ $KR_WIRELESS == "true" ]; then VERKKO_ESSID=$(verkko ssid); else VERKKO_ESSID=nada; fi
 			;;
 	esac
-		#. $KR_DIR_CFG/nets.sh
+		. $KR_DIR_CFG/nets.sh
 	else
 		virhe FILE nets.sh not found
 	fi
