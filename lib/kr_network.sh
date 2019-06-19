3006df224042fd773ed5561d1ed42268d2df4481
thowes <thw@iki.fi>
2019-06-19 10:22:25 +0300

    Edit kr_network.sh

--- lib/kr_network.sh
+++ lib/kr_network.sh
@@ -3,11 +3,7 @@ if [ $(asetus bool:hosted) != "true" ]; then
 	VERKKO_IP=$(verkko ip)
 	if [ $KR_WIRELESS == "true" ]; then VERKKO_ESSID=$(verkko ssid); else VERKKO_ESSID=nada; fi
 	if [ -f $KR_DIR_CFG/nets.sh ]; then
-	#case $KAYTTIS in
-		#darwin) # Using macos networks commands.
 		if [ $KR_WIRELESS == "true" ]; then VERKKO_ESSID=$(verkko ssid); else VERKKO_ESSID=nada; fi
-			#;;
-	#esac
 		. $KR_DIR_CFG/nets.sh
 	else
 		virhe FILE nets.sh not found
