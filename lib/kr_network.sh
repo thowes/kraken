a461ec441b2a8d626bfe147fec33a39330d42647
thowes <thw@iki.fi>
2019-06-19 10:19:30 +0300

    Edit kr_network.sh

--- lib/kr_network.sh
+++ lib/kr_network.sh
@@ -3,11 +3,9 @@ if [ $(asetus bool:hosted) != "true" ]; then
 	VERKKO_IP=$(verkko ip)
 	if [ $KR_WIRELESS == "true" ]; then VERKKO_ESSID=$(verkko ssid); else VERKKO_ESSID=nada; fi
 	if [ -f $KR_DIR_CFG/nets.sh ]; then
-	# Contents of KAYTTIS variable are defined in ymp.sh, to recognize the OS.
-	# Different network commands are available in different OSes.
 	#case $KAYTTIS in
 		#darwin) # Using macos networks commands.
-			if [ $KR_WIRELESS == "true" ]; then VERKKO_ESSID=$(verkko ssid); else VERKKO_ESSID=nada; fi
+		if [ $KR_WIRELESS == "true" ]; then VERKKO_ESSID=$(verkko ssid); else VERKKO_ESSID=nada; fi
 			#;;
 	#esac
 		. $KR_DIR_CFG/nets.sh
