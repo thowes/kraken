3f53b923c8adae411d793cbd859c4a6999f905f1
thowes <thw@iki.fi>
2019-06-20 19:16:12 +0300

    Add a function to kr_network.sh

--- lib/kr_network.sh
+++ lib/kr_network.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+verko_update() {
+	debug verkko update
+}
+
 if [ $(asetus bool:hosted) != "true" ]; then
 	VERKKO_IP=$(verkko ip)
 	if [ $KR_WIRELESS == "true" ]; then VERKKO_ESSID=$(verkko ssid); else VERKKO_ESSID=nada; fi
