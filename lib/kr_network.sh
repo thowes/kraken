77554a925ab378e6eba872a01c16e073289323bf
thowes <thw@iki.fi>
2019-06-26 10:44:03 +0300

    Edit kr_network.sh

--- lib/kr_network.sh
+++ lib/kr_network.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 verkko_update() {
+	VERKKO_IP=$(verkko ip)
 	if [ $KR_WIRELESS == "true" ]; then VERKKO_ESSID=$(verkko ssid); else VERKKO_ESSID=nada; fi
 	if [ -f $KR_DIR_CFG/nets.sh ]; then . $KR_DIR_CFG/nets.sh; else virhe FILE nets.sh not found; fi
 }
 
 if [ $(asetus bool:hosted) != "true" ]; then
-	VERKKO_IP=$(verkko ip)
 	verkko_update
 fi
