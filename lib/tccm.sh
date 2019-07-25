ded8e7d3884923eb710e16a0c0c4e321439bacb1
thowes <thw@iki.fi>
2019-07-26 01:52:56 +0300

    Edit tccm.sh

--- lib/tccm.sh
+++ lib/tccm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 tccm() {
-	debug TTCM $1 $2 $3; KR_TC_NAME=$(echo $3|awk -F "." '{print $1}'); KAYTTIS=$(kayttis); case $KAYTTIS in
+	kaiku "TTCM $1 $2 $3 $4 $5"; KR_TC_NAME=$(echo $3|awk -F "." '{print $1}'); KAYTTIS=$(kayttis); case $KAYTTIS in
 		cygwin) KRN_TC_DIR="/cygdrive";;
 		darwin) KRN_TC_DIR="/Volumes";;
 		ubuntu) KRN_TC_DIR="/media/$USER";;
@@ -20,6 +20,7 @@ tccm() {
 			*) virhe "Drive directory already in use.";;
 		esac
 	else
+		
 		case $1 in
 			ko) case $KAYTTIS in
 				cygwin|wsl) tc /l$2 /q /s /m ro /m rm /v $3 /p $KRN_PASSU;;
@@ -30,16 +31,20 @@ tccm() {
 				*) tc -m ts $3 -p $KRN_PASSU $KRN_TC_DIR/$2;;
 			esac;;
 			ro) case $KAYTTIS in
-				cygwin|wsl) tc /l$2 /q /s /m ro /m rm /v $3 /p $KRN_PASSU /k $4;;
-				*) tc -m ro $3 -p $KRN_PASSU -k $4 $KRN_TC_DIR/$2;;
+				cygwin|wsl) echo tc $APP /l$2 /q /s /m ro /m rm /v $3 /p $PASSU /k "$TCCM_K";;
+				*) if [ "_$4_" != "__" ]; then $APP -m ro $3 -p $PASSU -k "$4" $KRN_TC_DIR/$2; else $APP -m ro $3 -p $PASSU -k "" $KRN_TC_DIR/$2; fi;;
 			esac;;
 			rw) case $KAYTTIS in
-				cygwin|wsl) tc /l$2 /q /s /m ts /m rm /v $3 /p $KRN_PASSU /k $4;;
-				*) tc -m ts $3 -p $KRN_PASSU -k $4 $KRN_TC_DIR/$2;;
+				cygwin|wsl) tc /l$2 /q /s /m rm /m ts /v $3 /p $KRN_PASSU /k $4;;
+				*) tc -m rm -m ts $3 -p $KRN_PASSU -k $4 $KRN_TC_DIR/$2;;
 			esac;;
 			to) case $KAYTTIS in
 				cygwin|wsl) tc /l$2 /q /s /m ro /m rm /v $3 /p $KRN_PASSU /k $4;;
 				*) $APP -m ro $3 -p $PASSU -k $4 --mount $KRN_TC_DIR/$2;;
+			esac;;
+			ts) case $KAYTTIS in
+				cygwin|wsl) echo tc $APP /l$3 /q /s /m ro /m rm /v $3 /p $PASSU /k "$TCCM_K";;
+				*) if [ "_$4_" != "__" ]; then $APP -m ts $3 -p $PASSU -k "$4" $KRN_TC_DIR/$2; else $APP -m ts $3 -p $PASSU -k "" $KRN_TC_DIR/$2; fi  ;;
 			esac;;
 			tw) case $KAYTTIS in
 				cygwin|wsl) tc /l$2 /q /s /m ts /m rm /v $3 /p $KRN_PASSU /k $4;;
@@ -55,6 +60,6 @@ tccm() {
 			esac;;
 			*) virhe "TCCM command" $1 "not recognized.";;
 		esac
-		if [ -f $KRN_TC_DIR/$2/$KR_TC_NAME.sh ]; then . $KRN_TC_DIR/$2/$KR_TC_NAME.sh; else echo NO: $KRN_TC_DIR/$2/$KR_TC_NAME.sh; fi
+		#if [ -f $KRN_TC_DIR/$2/$KR_TC_NAME.sh ]; then . $KRN_TC_DIR/$2/$KR_TC_NAME.sh; else echo NO: $KRN_TC_DIR/$2/$KR_TC_NAME.sh; fi
 	fi
 }
