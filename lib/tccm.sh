ebbe7bc2c851861165e86de3f7a656bc207bf4bd
thowes <thw@iki.fi>
2019-08-10 12:21:55 +0100

    Edit tccm.sh

--- lib/tccm.sh
+++ lib/tccm.sh
@@ -29,10 +29,6 @@ tccm() {
 					fi;;
 				*) if [ "_$4_" != "__" ]; then $APP -m ro $3 -p $PASSU -k "$4" $KRN_TC_DIR/$2; else $APP -m ro $3 -p $PASSU -k "" $KRN_TC_DIR/$2; fi;;
 			esac;;
-			to) case $KAYTTIS in
-				cygwin|wsl) tc /l$2 /q /s /m ro /m rm /v $3 /p $KRN_PASSU /k $4;;
-				*) $APP -m ro $3 -p $PASSU -k $4 --mount $KRN_TC_DIR/$2;;
-			esac;;
 			ts) case $KAYTTIS in
 				cygwin|wsl) if [ "_$4_" != "__" ]; then echo $APP /m ro /m rm /v $3 /p $PASSU /k "$4" /l$3; else echo $APP /m ro /m rm /v $3 /p $PASSU /k \"\" /l$3; fi;;
 				*) if [ "_$4_" != "__" ]; then $APP -m ts $3 -p $PASSU -k "$4" $KRN_TC_DIR/$2; else $APP -m ts $3 -p $PASSU -k "" $KRN_TC_DIR/$2; fi  ;;
