5816690977c906c420dad00378e99c9eed92242c
thowes <thw@iki.fi>
2019-08-10 12:17:57 +0100

    Edit tccm.sh

--- lib/tccm.sh
+++ lib/tccm.sh
@@ -20,17 +20,17 @@ tccm() {
 		#esac
 	#else
 		#case $1 in
-			ko) case $KAYTTIS in
-				cygwin|wsl) tc /l$2 /q /s /m ro /m rm /v $3 /p $KRN_PASSU;;
-				*) tc -m ro $3 -p $KRN_PASSU $KRN_TC_DIR/$2;;
-			esac;;
 			kw) case $KAYTTIS in
 				cygwin|wsl) tc /l$2 /q /s /m ts /m rm /v $3 /p $KRN_PASSU;;
 				*) tc -m ts $3 -p $KRN_PASSU $KRN_TC_DIR/$2;;
 			esac;;
 			ro) case $KAYTTIS in
-				cygwin|wsl)if [ "_$4_" != "__" ]; then
-					echo $APP $TC_ARGS /m ro /m rm /v $3 /p $PASSU /k "$4" /l$2; else $APP $TC_ARGS /m ro /m rm /v \"$3\" /p $PASSU /k \"\" /l$2; fi;;
+				cygwin|wsl)
+					if [ "_$4_" != "__" ]; then
+						echo $APP $TC_ARGS /m ro /m rm /v $3 /p $PASSU /k "$4" /l$2
+					else
+						$APP $TC_ARGS /m ro /m rm /v \"$3\" /p $PASSU /k \"\" /l$2
+					fi;;
 				*) if [ "_$4_" != "__" ]; then $APP -m ro $3 -p $PASSU -k "$4" $KRN_TC_DIR/$2; else $APP -m ro $3 -p $PASSU -k "" $KRN_TC_DIR/$2; fi;;
 			esac;;
 			rw) case $KAYTTIS in
