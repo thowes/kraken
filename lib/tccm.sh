7d762b63683b751214f29cd6e55728574ff75d1d
thowes <thw@iki.fi>
2019-08-10 12:13:30 +0100

    Edit tccm.sh

--- lib/tccm.sh
+++ lib/tccm.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 tccm() {
-	echo "TTCM 1:$1 2:$2 3:$3 4:$4 5:$5"; pwd; KR_TC_NAME=$(echo $3|awk -F "." '{print $1}'); KAYTTIS=$(kayttis); case $KAYTTIS in
+	KR_TC_NAME=$(echo $3|awk -F "." '{print $1}'); KAYTTIS=$(kayttis); case $KAYTTIS in
 		cygwin) KRN_TC_DIR="/cygdrive";;
 		darwin) KRN_TC_DIR="/Volumes";;
 		ubuntu) KRN_TC_DIR="/media/$USER";;
 		wsl) KRN_TC_DIR="/mnt";;
-	esac
-	if [ -d $KRN_TC_DIR/$2/ ]; then
+	esac #echo "TTCM 1:$1 2:$2 3:$3 4:$4 5:$5"; #if [ -d $KRN_TC_DIR/$2/ ]; then
 		case $1 in
 			all) case $KAYTTIS in
-					cygwin|wsl) vc /q /s /d;;
+					cygwin|wsl) $APP $TC_ARGS /d;;
 					*) tc -d;;
 				esac;;
 			dm) case $KAYTTIS in
@@ -17,10 +16,10 @@ tccm() {
 					darwin) tc -d $KRN_TC_DIR/$2;;
 					*) tc -d $2;;
 				esac;;
-			*) virhe "Drive directory already in use.";;
-		esac
-	else
-		case $1 in
+			#*) virhe "Drive directory already in use.";;
+		#esac
+	#else
+		#case $1 in
 			ko) case $KAYTTIS in
 				cygwin|wsl) tc /l$2 /q /s /m ro /m rm /v $3 /p $KRN_PASSU;;
 				*) tc -m ro $3 -p $KRN_PASSU $KRN_TC_DIR/$2;;
@@ -30,7 +29,8 @@ tccm() {
 				*) tc -m ts $3 -p $KRN_PASSU $KRN_TC_DIR/$2;;
 			esac;;
 			ro) case $KAYTTIS in
-				cygwin|wsl) if [ "_$4_" != "__" ]; then echo $APP /m ro /m rm /v $3 /p $PASSU /k "$4" /l$2; else echo $APP /m ro /m rm /v $3 /p $PASSU /k \"\" /l$2; fi;;
+				cygwin|wsl)if [ "_$4_" != "__" ]; then
+					echo $APP $TC_ARGS /m ro /m rm /v $3 /p $PASSU /k "$4" /l$2; else $APP $TC_ARGS /m ro /m rm /v \"$3\" /p $PASSU /k \"\" /l$2; fi;;
 				*) if [ "_$4_" != "__" ]; then $APP -m ro $3 -p $PASSU -k "$4" $KRN_TC_DIR/$2; else $APP -m ro $3 -p $PASSU -k "" $KRN_TC_DIR/$2; fi;;
 			esac;;
 			rw) case $KAYTTIS in
@@ -60,5 +60,5 @@ tccm() {
 			*) virhe "TCCM command" $1 "not recognized.";;
 		esac
 		#if [ -f $KRN_TC_DIR/$2/$KR_TC_NAME.sh ]; then . $KRN_TC_DIR/$2/$KR_TC_NAME.sh; else echo NO: $KRN_TC_DIR/$2/$KR_TC_NAME.sh; fi
-	fi
+	#fi
 }
