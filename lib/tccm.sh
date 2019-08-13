dce5e922ce22705efa4f37f3bb3247e6a4b0f4a2
thowes <thw@iki.fi>
2019-08-14 00:37:27 +0100

    edit tccm.sh

--- lib/tccm.sh
+++ lib/tccm.sh
@@ -5,32 +5,32 @@ tccm() {
 		darwin) KRN_TC_DIR="/Volumes";;
 		ubuntu) KRN_TC_DIR="/media/$USER";;
 		wsl) KRN_TC_DIR="/mnt";;
-	esac #echo "TTCM 1:$1 2:$2 3:$3 4:$4 5:$5"; #if [ -d $KRN_TC_DIR/$2/ ]; then
+	esac; echo "TTCM 1:$1 2:$2 3:$3 4:$4 5:$5"; #if [ -d $KRN_TC_DIR/$2/ ]; then
 		case $1 in
 			all) case $KAYTTIS in
 					cygwin|wsl) $APP $TC_ARGS /d;;
 					*) tc -d;;
 				esac;;
 			dm) case $KAYTTIS in
-					cygwin|wsl) tc /q /s /d $2;;
+					cygwin|wsl) vc /q /s /d $2;;
 					darwin) tc -d $KRN_TC_DIR/$2;;
 					*) tc -d $2;;
 				esac;;
 			ro) case $KAYTTIS in
 				cygwin|wsl)
 					if [ "_$4_" != "__" ]; then
-						echo $APP $TC_ARGS /m ro /m rm /v $3 /p $PASSU /k "$4" /l$2
+						$APP $TC_ARGS /m ro /m rm /l $2 /v $3 /p $PASSU /k "$4"
 					else
-						$APP $TC_ARGS /m ro /m rm /v \"$3\" /p $PASSU /k \"\" /l$2
+						$APP $TC_ARGS /m ro /m rm /l $2 /v $3 /p $PASSU /k \"\"
 					fi;;
 				darwin) if [ "_$4_" != "__" ]; then $APP -m ro $3 -p $PASSU -k "$4" $KRN_TC_DIR/$2; else $APP -m ro $3 -p $PASSU -k "" $KRN_TC_DIR/$2; fi;;
 			esac;;
 			ts) case $KAYTTIS in
 				cygwin|wsl)
 					if [ "_$4_" != "__" ]; then
-						echo $APP /m ro /m rm /v $3 /p $PASSU /k "$4" /l$3
+						$APP $TC_ARGS /m ts /m rm /l $2 /v $3 /p $PASSU /k "$4"
 					else
-						echo $APP /m ro /m rm /v $3 /p $PASSU /k \"\" /l$3
+						$APP $TC_ARGS /m ts /m rm /l $2 /v $3 /p $PASSU /k \"\"
 					fi;;
 				darwin) if [ "_$4_" != "__" ]; then $APP -m ts $3 -p $PASSU -k "$4" $KRN_TC_DIR/$2; else $APP -m ts $3 -p $PASSU -k "" $KRN_TC_DIR/$2; fi  ;;
 			esac;;
