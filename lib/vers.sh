fec3b7425c2e4eff37324e49f6934b7a6c30dd2c
thowes <thw@iki.fi>
2019-10-10 14:08:10 +0300

    Edit lib/vers.sh

--- lib/vers.sh
+++ lib/vers.sh
@@ -7,11 +7,9 @@ vers_w() {
 			php) echo "php:" $(php -v) >> $DIR_CFG/versions.txt;;
 			psql) echo "psql:" $(psql -V) >> $DIR_CFG/versions.txt;;
 			python) echo "python:" $(python -V) >> $DIR_CFG/versions.txt;;
-			rsync) echo "rsync:" $(rsync --version|grep version) >> $DIR_CFG/versions.txt;;
 			rails) rails --version > $DIR_CFG/versions/$2.txt;;
 			ruby) ruby -v > $DIR_CFG/versions/$2.txt;;
 			svn) svn help | grep client > $DIR_CFG/versions/$2.txt;;
-			wget) echo "wget:" $(wget -V|grep ggdb|grep lib) >> $DIR_CFG/versions.txt;;
 			*) virhe VERS "parametria ei tunnistettu!";;
 		esac
 	fi
@@ -19,24 +17,26 @@ vers_w() {
 
 vers() {
 if [ -z "$DIR_CFG" ]; then DIR_CFG=$(asetus dir:cfg); fi
-	case $1 in
-		-v*) if [ -f $DIR_CFG/versions/$2.txt ]; then cat $DIR_CFG/versions/$2.txt; else virhe VERS "Parametre $2 not recognized!"; fi;;
-		-w*)
-			case $2 in
+case $1 in
+	-v*) if [ -f $DIR_CFG/versions.txt ]; then cat $DIR_CFG/versions.txt | grep $2; else virhe VERS "file versions.txt not found!"; fi;;
+	-w*)
+		case $2 in
 			apache) apache2 -v  > $DIR_CFG/versions/$2.txt;;
 				curl) curl -V | grep libcurl > $DIR_CFG/versions/$2.txt;;
-			git) git --version > $DIR_CFG/versions/$2.txt;;
-			heroku) heroku version > $DIR_CFG/versions/$2.txt;;
+			git) echo "git:" $(git --version) >> $DIR_CFG/versions.txt;;
+			heroku) echo "heroku:" $(heroku version) >> $DIR_CFG/versions.txt;;
 				gpg) echo "gpg:" $(gpg --version|grep GnuPG) >> $DIR_CFG/versions.txt;;
 				imagemagick) identify --version | grep Version > $DIR_CFG/versions/$2.txt;;
 				rbenv) rbenv version > $DIR_CFG/versions/$2.txt;;
-				*) if [ -f $DIR_CFG/versions.txt ]; then rm $DIR_CFG/versions.txt; fi; echo "versions.txt:" $(pvm) > $DIR_CFG/versions.txt; vers -w gpg; vers_w mysql; vers_w php; vers_w psql; vers_w python; vers_w rsync; vers_w wget;;
-			esac;; 
-		*)
+			rsync) echo "rsync:" $(rsync --version|grep version) >> $DIR_CFG/versions.txt;;
+			wget) echo "wget:" $(wget -V|grep Wget) >> $DIR_CFG/versions.txt;;
+			*) if [ -f $DIR_CFG/versions.txt ]; then rm $DIR_CFG/versions.txt; fi; echo "versions.txt:" $(pvm) > $DIR_CFG/versions.txt; vers -w git; vers -w gpg; vers -w heroku; vers_w mysql; vers_w php; vers_w psql; vers_w python; vers -w rsync; vers -w wget;;
+		esac;; 
+	*)
 	if [ -f $DIR_CFG/versions/$1.txt ]; then echo V36 $1 $2
 		cat $DIR_CFG/versions/$1.txt
 	else
 		virhe VERS "Parametria ei tunnistettu!"
 	fi;;
-	esac
+esac
 }
