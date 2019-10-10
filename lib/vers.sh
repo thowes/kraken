a3ec68e975f3614f780c6216bd7d8b6971850288
thowes <thw@iki.fi>
2019-10-10 13:10:00 +0300

    Edit lib/vers.sh

--- lib/vers.sh
+++ lib/vers.sh
@@ -3,15 +3,14 @@ if [ -z" $DIR_CFG" ]; then DIR_CFG=$(asetus dir:cfg); fi
 vers_w() {
 	if [ -d $DIR_CFG/versions/ ]; then
 		case $1 in
-			apache) apache2 -v  > $DIR_CFG/versions/$2.txt;;
 			git) git --version > $DIR_CFG/versions/$2.txt;;
 			heroku) heroku version > $DIR_CFG/versions/$2.txt;;
 			kraken) svn info ~/proj/kraken|grep Revision > $KR_DIR_CFG/versions/$1.txt;;
-			mysql) mysql -V > $KR_DIR_CFG/versions/$1.txt;;
-			php) php -v > $KR_DIR_CFG/versions/$1.txt;;
-			psql) psql -V > $KR_DIR_CFG/versions/$1.txt;;
-			python) python -V > $KR_DIR_CFG/versions/$1.txt;;
-			rsync) rsync --version|grep version > $KR_DIR_CFG/versions/$1.txt;;
+			mysql) echo "mysql:" $(mysql -V) >> $DIR_CFG/versions.txt;;
+			php) echo "php:" $(php -v) >> $DIR_CFG/versions.txt;;
+			psql) echo "psql:" $(psql -V) >> $DIR_CFG/versions.txt;;
+			python) echo "python:" $(python -V) >> $DIR_CFG/versions.txt;;
+			rsync) echo "rsync:" $(rsync --version|grep version) >> $DIR_CFG/versions.txt;;
 			rails) rails --version > $DIR_CFG/versions/$2.txt;;
 			ruby) ruby -v > $DIR_CFG/versions/$2.txt;;
 			svn) svn help | grep client > $DIR_CFG/versions/$2.txt;;
@@ -26,13 +25,16 @@ vers() {
 		-v*) if [ -f $DIR_CFG/versions/$2.txt ]; then cat $DIR_CFG/versions/$2.txt; else virhe VERS "Parametre $2 not recognized!"; fi;;
 		-w*)
 			case $2 in
+			apache) apache2 -v  > $DIR_CFG/versions/$2.txt;;
 				curl) curl -V | grep libcurl > $DIR_CFG/versions/$2.txt;;
+				gpg) echo "gpg:" $(gpg --version|grep GnuPG) >> $DIR_CFG/versions.txt;;
 				imagemagick) identify --version | grep Version > $DIR_CFG/versions/$2.txt;;
 				rbenv) rbenv version > $DIR_CFG/versions/$2.txt;;
+				*) pvm > $DIR_CFG; vers -w gpg; vers_w mysql; vers_w php; vers_w psql; vers_w python; vers_w rsync;;
 			esac;;
 		*)
-	if [ -f $KR_DIR_CFG/versions/$1.txt ]; then
-		cat $KR_DIR_CFG/versions/$1.txt
+	if [ -f $DIR_CFG/versions/$1.txt ]; then
+		cat $DIR_CFG/versions/$1.txt
 	else
 		virhe VERS "Parametria ei tunnistettu!"
 	fi;;
