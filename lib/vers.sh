489e85dcad4f46110c5dd5d7336b1fad61130428
thowes <thw@iki.fi>
2019-10-15 11:12:59 +0300

    Edit vers.sh

--- lib/vers.sh
+++ lib/vers.sh
@@ -2,7 +2,6 @@
 vers_w() {
 	if [ -d $DIR_CFG/versions/ ]; then
 		case $1 in
-			kraken) echo "kraken:" $(svn info ~/proj/kraken|grep Revision) >> $DIR_CFG/versions.txt;;
 			*) virhe VERS "parametria ei tunnistettu!";;
 		esac
 	fi
@@ -20,6 +19,7 @@ case $1 in
 			heroku) echo "heroku:" $(heroku version) >> $DIR_CFG/versions.txt;;
 			gpg) echo "gpg:" $(gpg --version|grep GnuPG) >> $DIR_CFG/versions.txt;;
 			imagemagick) echo "imagemagick:" $(identify --version|grep Version) >> $DIR_CFG/versions.txt;;
+			kraken) echo "kraken:" $(svn info ~/proj/kraken|grep Revision) >> $DIR_CFG/versions.txt;;
 			mysql) echo "mysql:" $(mysql -V) >> $DIR_CFG/versions.txt;;
 			php) echo "php:" $(php -v) >> $DIR_CFG/versions.txt;;
 			psql) echo "psql:" $(psql -V) >> $DIR_CFG/versions.txt;;
@@ -33,14 +33,15 @@ case $1 in
 			*)
 				if [ -f $DIR_CFG/versions.txt ]; then rm $DIR_CFG/versions.txt; fi
 				echo "versions.txt:" $(pvm) > $DIR_CFG/versions.txt
-				vers -w apache; vers -w curl; vers -w git; vers -w gpg; vers -w heroku; vers -w imagemagick; vers -w mysql; vers -w php; vers -w psql; vers -w python; vers -w rails; vers -w rbenv; vers -w rsync; vers -w ruby; vers -w svn; vers -w wget
+				vers -w curl; vers -w git; vers -w gpg; vers -w heroku; vers -w imagemagick; vers -w mysql; vers -w python; vers -w rails; vers -w rbenv; vers -w rsync; vers -w ruby; vers -w wget
+				#vers -w apache; vers -w php; vers -w psql; vers -w svn;
 				;;
 		esac;; 
 	*)
-	if [ -f $DIR_CFG/versions.txt ]; then
-		cat $DIR_CFG/versions.txt | grep "$1"
-	else
-		virhe "VERS versions.txt not found!"
-	fi;;
+		if [ -z "$1" ]; then
+			vers -w all
+		else
+			if [ -f $DIR_CFG/versions.txt ]; then cat $DIR_CFG/versions.txt | grep "$1"; else virhe "VERS versions.txt not found!"; fi
+		fi;;
 esac
 }
