10b0d2f1db628868d00c397a014ea000f48a9a46
thowes <thw@iki.fi>
2019-10-15 11:17:36 +0300

    Edit vers.sh

--- lib/vers.sh
+++ lib/vers.sh
@@ -1,29 +1,21 @@
 #!/bin/bash
-vers_w() {
-	if [ -d $DIR_CFG/versions/ ]; then
-		case $1 in
-			*) virhe VERS "parametria ei tunnistettu!";;
-		esac
-	fi
-}
-
 vers() {
 if [ -z "$DIR_CFG" ]; then DIR_CFG=$(asetus dir:cfg); fi
 case $1 in
 	-v*) if [ -f $DIR_CFG/versions.txt ]; then cat $DIR_CFG/versions.txt | grep $2; else virhe VERS "file versions.txt not found!"; fi;;
 	-w*)
 		case $2 in
-			apache) echo "apache:" $(apache2 -v) >> $DIR_CFG/versions.txt;;
+			apa*) echo "apache:" $(apache2 -v) >> $DIR_CFG/versions.txt;;
 			curl) echo "curl:" $(curl -V | grep libcurl) >> $DIR_CFG/versions.txt;;
 			git) echo "git:" $(git --version) >> $DIR_CFG/versions.txt;;
 			heroku) echo "heroku:" $(heroku version) >> $DIR_CFG/versions.txt;;
 			gpg) echo "gpg:" $(gpg --version|grep GnuPG) >> $DIR_CFG/versions.txt;;
-			imagemagick) echo "imagemagick:" $(identify --version|grep Version) >> $DIR_CFG/versions.txt;;
+			im*) echo "imagemagick:" $(identify --version|grep Version) >> $DIR_CFG/versions.txt;;
 			kraken) echo "kraken:" $(svn info ~/proj/kraken|grep Revision) >> $DIR_CFG/versions.txt;;
-			mysql) echo "mysql:" $(mysql -V) >> $DIR_CFG/versions.txt;;
+			my*) echo "mysql:" $(mysql -V) >> $DIR_CFG/versions.txt;;
 			php) echo "php:" $(php -v) >> $DIR_CFG/versions.txt;;
-			psql) echo "psql:" $(psql -V) >> $DIR_CFG/versions.txt;;
-			python) echo "python:" $(python -V|grep Python) >> $DIR_CFG/versions.txt;;
+			ps*) echo "psql:" $(psql -V) >> $DIR_CFG/versions.txt;;
+			py*) echo "python:" $(python -V|grep Python) >> $DIR_CFG/versions.txt;;
 			rails) echo "rails:" $(rails --version) >> $DIR_CFG/versions.txt;;
 			rbenv) echo "rbenv:" $(rbenv version) >> $DIR_CFG/versions.txt;;
 			rsync) echo "rsync:" $(rsync --version|grep version) >> $DIR_CFG/versions.txt;;
@@ -33,7 +25,7 @@ case $1 in
 			*)
 				if [ -f $DIR_CFG/versions.txt ]; then rm $DIR_CFG/versions.txt; fi
 				echo "versions.txt:" $(pvm) > $DIR_CFG/versions.txt
-				vers -w curl; vers -w git; vers -w gpg; vers -w heroku; vers -w imagemagick; vers -w mysql; vers -w python; vers -w rails; vers -w rbenv; vers -w rsync; vers -w ruby; vers -w wget
+				vers -w curl; vers -w git; vers -w gpg; vers -w heroku; vers -w im; vers -w my; vers -w py; vers -w rails; vers -w rbenv; vers -w rsync; vers -w ruby; vers -w wget
 				#vers -w apache; vers -w php; vers -w psql; vers -w svn;
 				;;
 		esac;; 
