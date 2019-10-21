115ece664e7a261423b88fb8e584aac51468bed3
thowes <thw@iki.fi>
2019-10-21 17:28:27 +0300

    Edit val.sh

--- lib/val.sh
+++ lib/val.sh
@@ -3,10 +3,10 @@ valitse() {
 DIR_CFG=$(asetus dir:cfg)
 #prints to screen list projects valid for current location
 cat $DIR_CFG/proj.csv|\grep $1|awk -F, '{ print " "$1"\t" $2 }'
-	KR_PROJ_TEMP="def"
-	KR_PROJ_TEST=" "
-	read -p "PROJECT " -r KR_PROJ_TEMP
-	KR_PROJ_TEST=$(echo $KR_PROJ_TEMP)
+KR_PROJ_TEMP="def"
+KR_PROJ_TEST=" "
+read -p "PROJECT " -r KR_PROJ_TEMP
+KR_PROJ_TEST=$(echo $KR_PROJ_TEMP)
 	if [[ -n "${KR_PROJ_TEST/[ ]*\n/}" ]]; then
 		#execute if the the variable is not empty and contains non space characters
 		PROJECT=$KR_PROJ_TEMP
