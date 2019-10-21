161448d95d83e3d45c3d435e27ac87f573818c6b
thowes <thw@iki.fi>
2019-10-21 17:45:50 +0300

    Edit val.sh

--- lib/val.sh
+++ lib/val.sh
@@ -7,16 +7,16 @@ KR_PROJ_TEMP="def"
 KR_PROJ_TEST=" "
 read -p "PROJECT " -r KR_PROJ_TEMP
 KR_PROJ_TEST=$(echo $KR_PROJ_TEMP)
-	if [[ -n "${KR_PROJ_TEST/[ ]*\n/}" ]]; then
+if [[ -n "${KR_PROJ_TEST/[ ]*\n/}" ]]; then
 		#execute if the the variable is not empty and contains non space characters
 		PROJECT=$KR_PROJ_TEMP
 		debug PROJ is $KR_PROJ_TEMP "." 
-	else
+else
 		#execute if the variable is empty or contains only spaces
 		PROJECT="def"
 		debug PROJ $KR_PROJ_TEST is "empty." 
-	fi
+fi
 KR_DIRPO_LINE=$(cat $DIR_CFG/proj.csv|\grep $PROJECT)
 KR_DIRPO=$(echo $KR_DIRPO_LINE|awk -F, '{print $4}')
-echo PROJECT=$PROJECT \n KR_DIRPO_LINE=$KR_DIRPO_LINE \n KR_DIRPO=$KR_DIRPO > $(asetus dir:temp)/$(verkko tty).proj
+printf "PROJECT=$PROJECT \nKR_DIRPO_LINE=$KR_DIRPO_LINE \nKR_DIRPO=$KR_DIRPO" > $(asetus dir:temp)/$(verkko tty).proj
 }
