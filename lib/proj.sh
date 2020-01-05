d6ec3da95fbbc87c67b015ccbf4e1318a9699f46
thowes <thw@iki.fi>
2020-01-05 22:16:55 +0200

    Clean commented code from lib/proj.sh

--- lib/proj.sh
+++ lib/proj.sh
@@ -13,6 +13,7 @@ projekti_update() {
 # reads list projects (proj.csv) and jumps to the project folder.
 projekti() {
 	case $1 in
+		-go) KRN_PROJ_FUNCTION="-p"; KRN_PROJ_INPUT=$2;;
 		-o) KRN_PROJ_FUNCTION="-o"; KRN_PROJ_INPUT=$2;;
 		-p) KRN_PROJ_FUNCTION="-p"; KRN_PROJ_INPUT=$2;;
 		-st) KRN_PROJ_FUNCTION="-st"; KRN_PROJ_INPUT=$2;;
@@ -22,7 +23,6 @@ projekti() {
 		*) KRN_PROJ_FUNCTION="-p"; KRN_PROJ_INPUT=$1;;
 	esac
 	KR_DIRPO=$(projekti-dir -d $KRN_PROJ_INPUT)
-	KR_PROJECTS_COUNT=$(cat $KR_DIR_CFG/proj.csv | \grep $KRN_PROJ_INPUT | wc -l | awk '{ print $1 }')
 	#case $KR_PROJECTS_COUNT in
 	#	0) virhe "Project keyword" $KRN_PROJ_INPUT "not found" "in project file.";;
 	#	1)
@@ -32,17 +32,17 @@ projekti() {
 	case $KR_DIRPO_TEMP in
 		~*) KR_DIRPO=$KR_DIRPO_HOME$(echo $KR_DIRPO_TEMP | awk -F~ '{ print$2 }');;
 	esac
-			if [ -d $KR_DIRPO ]; then
-				\cd $KR_DIRPO
-				case $KRN_PROJ_FUNCTION in
+	if [ -d $KR_DIRPO ]; then
+		\cd $KR_DIRPO
+		case $KRN_PROJ_FUNCTION in
 					-o) open .;;
 					-u) if [ -d .git/ ]; then projekti_update; fi;;
 					*) if [ -d .git/ ]; then git status --short; else if [ -d ../.git/ ]; then git status --short; else if [ -d ../../.git/ ]; then git status --short; else ls; fi; fi; fi;;
-				esac
+		esac
 				compu_proj $KRN_PROJ_FUNCTION $KRN_PROJ_INPUT
-			else 
+	else 
 				virhe PROJ $KRN_PROJ_INPUT $KR_DIRPO "is not a directory!"
-			fi #;;
+	fi
 		#*) virhe "Project keyword" $KRN_PROJ_INPUT "found multiple times in project file.";;
 	#esac
 }
