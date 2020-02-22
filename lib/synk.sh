0449673495675c9afc5ef46fe131087118c70185
thowes <thw@iki.fi>
2020-02-22 20:10:35 +0200

    Edit tabs and variable names for readability in synk.sh

--- lib/synk.sh
+++ lib/synk.sh
@@ -4,11 +4,11 @@ KR_DIR_CFG=$(asetus dir:cfg); CFG=$(asetus dir:cfg)
 KR_DIR_EXCL=$(asetus dir:excl); EXCL=$(asetus dir:excl)
 case $1 in
 	csv) synkronoi ssh $2;;
-	dwl) if [ -f $KR_DIR_CFG/dwl.csv ]; then
-			KR_SYNK_N=_$(cat $KR_DIR_CFG/dwl.csv | \grep $2 | wc -l | tr -s ' ' | tr " " "_" )_
+	dwl) if [ -f $CFG/dwl.csv ]; then
+			KR_SYNK_N=_$(cat $CFG/dwl.csv | \grep $2 | wc -l | tr -s ' ' | tr " " "_" )_
 			case $KR_SYNK_N in
-					*_0_) virhe "SY11 $1: information not found.";;
-					*_1_) KR_SYNK_LINE=$(cat $KR_DIR_CFG/dwl.csv | \grep $2)
+				*_0_) virhe "SY11 $1: information not found.";;
+				*_1_) KR_SYNK_LINE=$(cat $CFG/dwl.csv | \grep $2)
 						KR_SYNK_EXCL=$KR_DIR_EXCL/$(echo $KR_SYNK_LINE|awk -F\; '{print $1}').lst
 						if [ -f $KR_SYNK_EXCL ]; then debug "file exists."; else KR_SYNK_EXCL=$KR_DIR_EXCL/default.lst; fi
 						KR_SYNK_SERVER=$(echo $KR_SYNK_LINE|awk -F\; '{print $2}')
@@ -27,13 +27,13 @@ case $1 in
 								rsync $KR_SYNK_PARAM $KR_SYNK_USER@$KR_SYNK_SERVER:$KR_SYNK_RDIR $KR_SYNK_LDIR --exclude-from $KR_SYNK_EXCL
 							else virhe "ALREADY done backup today" $2; fi
 						else virhe "SY29: DIR" $KR_SYNK_LDIR not found.; fi;;
-					*) virhe "found too many sites ($KR_SYNK_N) .";;
+				*) virhe "found too many sites ($KR_SYNK_N) .";;
 			esac; else virhe "csv file not found."; fi;;
-	ssh) if [ -f $KR_DIR_CFG/dwl.csv ] && [ -f $KR_DIR_CFG/upl.csv ]; then
-			KR_SYNK_N=_$(cat $KR_DIR_CFG/*l.csv | \grep $2 | wc -l | tr -s ' ' | tr " " "_" )_
+	ssh) if [ -f $CFG/dwl.csv ] && [ -f $CFG/upl.csv ]; then
+			KR_SYNK_N=_$(cat $CFG/*l.csv | \grep $2 | wc -l | tr -s ' ' | tr " " "_" )_
 			case $KR_SYNK_N in
 					*_0_) virhe "information not found.";;
-					*_1_) KR_SYNK_LINE=$(cat $KR_DIR_CFG/*l.csv | \grep $2)
+					*_1_) KR_SYNK_LINE=$(cat $CFG/*l.csv | \grep $2)
 						KR_SYNK_SERVER=$(echo $KR_SYNK_LINE|awk -F\; '{print $2}')
 						KR_SYNK_USER=$(echo $KR_SYNK_LINE|awk -F\; '{print $3}')
 						kaiku SY $1 $2 $KR_SYNK_USER@$KR_SYNK_SERVER
@@ -43,11 +43,11 @@ case $1 in
 					*) virhe "found too many sites (" "$KR_SYNK_N" ")." ;;
 			esac; else virhe "csv file(s) not found."; fi;;
 	#syn) synkronoi upl $2;;
-	upl) if [ -f $KR_DIR_CFG/upl.csv ]; then
-			KR_SYNK_N=_$(cat $KR_DIR_CFG/upl.csv | \grep $2 | wc -l | tr -s ' ' | tr " " "_" )_
+	upl) if [ -f $CFG/upl.csv ]; then
+			KR_SYNK_N=_$(cat $CFG/upl.csv | \grep $2 | wc -l | tr -s ' ' | tr " " "_" )_
 			case $KR_SYNK_N in
 					*_0_) virhe "information not found.";;
-					*_1_) KR_SYNK_LINE=$(cat $KR_DIR_CFG/upl.csv | \grep $2)
+					*_1_) KR_SYNK_LINE=$(cat $CFG/upl.csv | \grep $2)
 						KR_SYNK_EXCL=$KR_DIR_EXCL/$(echo $KR_SYNK_LINE|awk -F\; '{print $1}').lst
 						if [ -f $KR_SYNK_EXCL ]; then debug "file exists."; else KR_SYNK_EXCL=$KR_DIR_EXCL/default.lst; fi
 						KR_SYNK_SERVER=$(echo $KR_SYNK_LINE|awk -F\; '{print $2}')
