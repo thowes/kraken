d4c60f804b3734121849a70ba8b915cdfc7cc6d6
thowes <thw@iki.fi>
2020-03-03 15:14:02 +0200

    Edit variable names and tabs in synk.sh

--- lib/synk.sh
+++ lib/synk.sh
@@ -48,14 +48,14 @@ case $1 in
 			KR_SYNK_N=_$(cat $CFG/upl.csv | \grep $2 | wc -l | tr -s ' ' | tr " " "_" )_
 			case $KR_SYNK_N in
 				*_0_) virhe "information not found.";;
-				*_1_) KR_SYNK_LINE=$(cat $CFG/upl.csv | \grep $2)
-						EXCL=$KR_DIR_EXCL/$(echo $KR_SYNK_LINE|awk -F\; '{print $1}').lst
-						if [ -f $EXCL ]; then debug "file exists."; else EXCL=$KR_DIR_EXCL/default.lst; fi
-						KR_SYNK_SERVER=$(echo $KR_SYNK_LINE|awk -F\; '{print $2}')
-						KR_SYNK_USER=$(echo $KR_SYNK_LINE|awk -F\; '{print $3}')
-						KR_SYNK_PARAM=$(echo $KR_SYNK_LINE|awk -F\; '{print $4}')
-						KR_SYNK_LDIR=$(echo $KR_SYNK_LINE|awk -F\; '{print $5}')
-						KR_SYNK_RDIR=$(echo $KR_SYNK_LINE|awk -F\; '{print $6}')
+				*_1_) LINE=$(cat $CFG/upl.csv | \grep $2)
+					EXCL=$KR_DIR_EXCL/$(echo $LINE|awk -F\; '{print $1}').lst
+					if [ -f $EXCL ]; then debug "file exists."; else EXCL=$KR_DIR_EXCL/default.lst; fi
+					KR_SYNK_SERVER=$(echo $LINE|awk -F\; '{print $2}')
+					KR_SYNK_USER=$(echo $LINE|awk -F\; '{print $3}')
+					KR_SYNK_PARAM=$(echo $LINE|awk -F\; '{print $4}')
+					KR_SYNK_LDIR=$(echo $LINE|awk -F\; '{print $5}')
+					KR_SYNK_RDIR=$(echo $LINE|awk -F\; '{print $6}')
 					if [ -d $KR_SYNK_LDIR ]; then
 							# Getting the last row of backup.log and splitting it to retrieve the part of the string for latest backup date.
 							BU_LATEST_DATE=$(latest $KR_SYNK_LDIR|awk '{print $1}'); BU_TODAY_DATE=$(date +"%F")
