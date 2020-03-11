7c674935559dbdfac452c3b6983909b92d62e83b
thowes <thw@iki.fi>
2020-03-11 20:27:00 +0200

    Edit variable names in lib/val.sh

--- lib/val.sh
+++ lib/val.sh
@@ -3,14 +3,14 @@ valitse() {
 CFG=$(asetus dir:cfg)
 #prints to screen list projects valid for current location
 cat $CFG/proj.csv|\grep $1|awk -F, '{ print " "$1"\t" $2 }'
-KR_PROJ_TEMP="def"
+TEMP="def"
 KR_PROJ_TEST=" "
-read -p "PROJECT " -r KR_PROJ_TEMP
-KR_PROJ_TEST=$(echo $KR_PROJ_TEMP)
+read -p "PROJECT " -r TEMP
+KR_PROJ_TEST=$(echo $TEMP)
 if [[ -n "${KR_PROJ_TEST/[ ]*\n/}" ]]; then
 		#execute if the the variable is not empty and contains non space characters
-		PROJECT=$KR_PROJ_TEMP
-		debug PROJ is $KR_PROJ_TEMP "." 
+		PROJECT=$TEMP
+		debug PROJ is $TEMP "." 
 else
 		#execute if the variable is empty or contains only spaces
 		PROJECT="def"
